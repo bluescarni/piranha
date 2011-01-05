9a9bf38ad2bc6b15337582a492bc692283d0cd36
Francesco Biscani <bluescarni@gmail.com>
2011-01-05 12:41:01 +0100

    Added missing initialization of data member.

--- src/thread_barrier.cpp
+++ src/thread_barrier.cpp
@@ -36,7 +36,7 @@ namespace piranha
  * @throws std::invalid_argument if <tt>count == 0</tt>.
  * @throws std::system_error in case of failure(s) by threading primitives.
  */
-thread_barrier::thread_barrier(unsigned count):m_threshold(count),m_count(count),m_generation(0)
+thread_barrier::thread_barrier(unsigned count):m_mutex(),m_cond(),m_threshold(count),m_count(count),m_generation(0)
 {
 	if (count == 0) {
 		piranha_throw(std::invalid_argument,"count cannot be zero");
