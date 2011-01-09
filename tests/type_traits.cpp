43e9a6acaca9b6728939d2f7db62bf2fb66860aa
Francesco Biscani <bluescarni@gmail.com>
2011-01-09 02:33:06 +0100

    Added test for type traits.

---
+++ tests/type_traits.cpp
@@ -0,0 +1,47 @@
+/***************************************************************************
+ *   Copyright (C) 2009-2011 by Francesco Biscani                          *
+ *   bluescarni@gmail.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "../src/type_traits.hpp"
+
+#define BOOST_TEST_MODULE type_traits_test
+#include <boost/test/unit_test.hpp>
+
+#include <string>
+
+using namespace piranha;
+
+BOOST_AUTO_TEST_CASE(is_trivially_copyable_test)
+{
+	struct trivial0 {};
+	BOOST_CHECK_EQUAL(is_trivially_copyable<trivial0>::value,true);
+	struct trivial1
+	{
+		trivial1():m_value(1) {}
+		~trivial1() {m_value = 0;}
+		double m_value;
+	};
+	BOOST_CHECK_EQUAL(is_trivially_copyable<trivial1>::value,true);
+	struct nontrivial0
+	{
+		nontrivial0():m_str("hello") {}
+		std::string m_str;
+	};
+	BOOST_CHECK_EQUAL(is_trivially_copyable<nontrivial0>::value,false);
+}
