0f02189d3a3d4d01124e57045eaf7044e617fe86
Francesco Biscani <bluescarni@gmail.com>
2011-01-21 12:22:28 +0100

    Add small helper class for use in CRTP.

--- src/crtp_helper.hpp
+++ src/crtp_helper.hpp
@@ -18,42 +18,43 @@
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
 
-#ifndef PIRANHA_PIRANHA_HPP
-#define PIRANHA_PIRANHA_HPP
+#ifndef PIRANHA_CRTP_HELPER_HPP
+#define PIRANHA_CRTP_HELPER_HPP
 
-/** \file piranha.hpp
- * Global piranha header file.
- * 
- * Include this file to import piranha's entire public interface.
- */
+namespace piranha
+{
 
-/// Root piranha namespace.
+/// CRTP helper class.
 /**
- * \todo Check if in the end destructors will be implicitly marked as noexcept(true) in the final c++0x standard,
- * and if this is not the case add it.
+ * Small class that provides convenience methods when using the curiously-recurring
+ * template pattern (CRTP). Inheriting from this class will allow for easy access
+ * to the derived class in the CRTP.
+ * 
+ * @see http://en.wikipedia.org/wiki/Curiously_recurring_template_pattern
+ * 
+ * @author Francesco Biscani (bluescarni@gmail.com)
  */
-namespace piranha {}
+template <typename Derived>
+struct crtp_helper
+{
+	/// Pointer to derived class.
+	/**
+	 * @return pointer to the derived class.
+	 */
+	Derived *derived()
+	{
+		return static_cast<Derived *>(this);
+	}
+	/// Const pointer to the derived class.
+	/**
+	 * @return const pointer to the derived class.
+	 */
+	Derived const *derived() const
+	{
+		return static_cast<Derived const *>(this);
+	}
+};
 
-// NOTES FOR DOCUMENTATION:
-// - thread safety: assume none unless specified
-// - bad_cast due to boost numeric cast: say that it might be thrown in many places, too cumbersome
-//   to document every occurrence.
-// - c++0x features not implemented yet in GCC latest version: what impact they have and piranha_* macros used
-//   to signal/emulate them.
-
-#include "config.hpp"
-#include "crtp_helper.hpp"
-#include "cvector.hpp"
-#include "exceptions.hpp"
-#include "hop_table.hpp"
-#include "integer.hpp"
-#include "mf_int.hpp"
-#include "runtime_info.hpp"
-#include "settings.hpp"
-#include "symbol.hpp"
-#include "thread_barrier.hpp"
-#include "thread_group.hpp"
-#include "thread_management.hpp"
-#include "type_traits.hpp"
+}
 
 #endif
