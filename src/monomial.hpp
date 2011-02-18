39afc65b4f28ac934204654bd453f1d322939fb0
Francesco Biscani <bluescarni@gmail.com>
2011-02-18 15:39:47 +0100

    Added monomial class.

---
+++ src/monomial.hpp
@@ -0,0 +1,100 @@
+/***************************************************************************
+ *   Copyright (C) 2009-2011 by Francesco Biscani                          *
+ *   bluescarni@gmail.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef PIRANHA_MONOMIAL_HPP
+#define PIRANHA_MONOMIAL_HPP
+
+#include <initializer_list>
+#include <unordered_set>
+
+#include "array_key.hpp"
+
+namespace piranha
+{
+
+/// Monomial class.
+/**
+ * This class extends piranha::array_key to define a series key type suitable as monomial in polynomial terms.
+ * 
+ * @author Francesco Biscani (bluescarni@gmail.com)
+ */
+template <typename T>
+class monomial: public array_key<T,monomial<T>>
+{
+		typedef array_key<T,monomial<T>> base;
+	public:
+		/// Defaulted default constructor.
+		monomial() = default;
+		/// Defaulted copy constructor.
+		monomial(const monomial &) = default;
+		/// Defaulted move constructor.
+		monomial(monomial &&) = default;
+		/// Constructor from initializer list.
+		/**
+		 * @param[in] list initializer list.
+		 * 
+		 * @see piranha::array_key's constructor from initializer list.
+		 */
+		monomial(std::initializer_list<T> list):base(list) {}
+		/// Generic constructor.
+		/**
+		 * Will forward its arguments to one of the constructors of piranha::array_key.
+		 * 
+		 * @param[in] params arguments used for construction.
+		 * 
+		 * @throws unspecified any exception thrown by the invoked <tt>piranha::array_key</tt>'s constructor.
+		 */
+		template <typename... Args>
+		explicit monomial(Args && ... params):base(std::forward<Args>(params)...) {}
+		/// Defaulted destructor.
+		~monomial() = default;
+		/// Generic assignment operator.
+		/**
+		 * Will forward the assignment operator to the base class.
+		 * 
+		 * @param[in] other assignment argument.
+		 * 
+		 * @return reference to \p this.
+		 * 
+		 * @throws unspecified any exception thrown by the invoked <tt>piranha::array_key</tt>'s assignment operator.
+		 */
+		template <typename U>
+		monomial &operator=(U &&other)
+		{
+			base::operator=(std::forward<U>(other));
+			return *this;
+		}
+};
+
+}
+
+namespace std
+{
+
+/// Specialisation of \p std::hash for piranha::monomial.
+/**
+ * Functionally equivalent to the \p std::hash specialisation for piranha::array_key.
+ */
+template <typename T>
+struct hash<piranha::monomial<T>>: public hash<piranha::array_key<T,piranha::monomial<T>>> {};
+
+}
+
+#endif
---
+++ tests/monomial.cpp
@@ -0,0 +1,81 @@
+/***************************************************************************
+ *   Copyright (C) 2009-2011 by Francesco Biscani                          *
+ *   bluescarni@gmail.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "../src/monomial.hpp"
+
+#define BOOST_TEST_MODULE monomial_test
+#include <boost/test/unit_test.hpp>
+
+#include <boost/mpl/for_each.hpp>
+#include <boost/mpl/vector.hpp>
+
+#include "../src/integer.hpp"
+
+using namespace piranha;
+
+typedef boost::mpl::vector<unsigned,integer> expo_types;
+
+// Constructors, assignments and element access.
+struct constructor_tester
+{
+	template <typename T>
+	void operator()(const T &)
+	{
+		typedef monomial<T> monomial_type;
+		monomial_type m0;
+		BOOST_CHECK_NO_THROW(monomial_type{});
+		BOOST_CHECK_NO_THROW(monomial_type{monomial_type{}});
+		BOOST_CHECK_NO_THROW(monomial_type{m0});
+		// From init list.
+		monomial_type m1{T(0),T(1),T(2),T(3)};
+		BOOST_CHECK_EQUAL(m1.size(),static_cast<decltype(m1.size())>(4));
+		for (typename monomial_type::size_type i = 0; i < m1.size(); ++i) {
+			BOOST_CHECK_EQUAL(m1[i],i);
+			BOOST_CHECK_NO_THROW(m1[i] = i + 1);
+			BOOST_CHECK_EQUAL(m1[i],i + 1);
+		}
+		BOOST_CHECK_NO_THROW(m0 = m1);
+		BOOST_CHECK_NO_THROW(m0 = std::move(m1));
+	}
+};
+
+BOOST_AUTO_TEST_CASE(monomial_constructor_test)
+{
+	boost::mpl::for_each<expo_types>(constructor_tester());
+}
+
+struct hash_tester
+{
+	template <typename T>
+	void operator()(const T &)
+	{
+		typedef monomial<T> monomial_type;
+		monomial_type m0;
+		BOOST_CHECK_EQUAL(m0.hash(),std::size_t());
+		BOOST_CHECK_EQUAL(m0.hash(),std::hash<monomial_type>()(m0));
+		monomial_type m1{T(0),T(1),T(2),T(3)};
+		BOOST_CHECK_EQUAL(m1.hash(),std::hash<monomial_type>()(m1));
+	}
+};
+
+BOOST_AUTO_TEST_CASE(monomial_hash_test)
+{
+	boost::mpl::for_each<expo_types>(hash_tester());
+}
