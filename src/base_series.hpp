2e07627d07d4f0840d578036883b1f460f012a77
Francesco Biscani <bluescarni@gmail.com>
2011-03-21 18:09:32 +0100

    Added files for base series class and echelon descriptor.

---
+++ src/base_series.hpp
@@ -0,0 +1,260 @@
+/***************************************************************************
+ *   Copyright (C) 2009-2011 by Francesco Biscani                          *
+ *   bluescarni@gmail.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef PIRANHA_BASE_SERIES_HPP
+#define PIRANHA_BASE_SERIES_HPP
+
+#include <boost/concept/assert.hpp>
+#include <iostream>
+#include <stdexcept>
+#include <type_traits>
+
+#include "base_term.hpp"
+#include "concepts/crtp.hpp"
+#include "concepts/term.hpp"
+#include "config.hpp"
+#include "debug_access.hpp"
+#include "detail/base_series_fwd.hpp"
+#include "echelon_descriptor.hpp"
+#include "hop_table.hpp"
+#include "type_traits.hpp"
+
+namespace piranha
+{
+
+/// Base series class.
+/**
+ * \section type_requirements Type requirements
+ * 
+ * - \p Term must be a model of piranha::concept::Term.
+ * - \p Derived must be a model of piranha::concept::CRTP.
+ * 
+ * @author Francesco Biscani (bluescarni@gmail.com)
+ */
+template <typename Term, typename Derived = void>
+class base_series: detail::base_series_tag
+{
+		BOOST_CONCEPT_ASSERT((concept::Term<Term>));
+		BOOST_CONCEPT_ASSERT((concept::CRTP<Derived>));
+	public:
+		/// Alias for term type.
+		typedef Term term_type;
+	private:
+		// Make friend with debugging class.
+		template <typename T>
+		friend class debug_access;
+		// Hash functor for term_type.
+		struct term_hasher
+		{
+			std::size_t operator()(const term_type &term) const
+			{
+				return term.hash();
+			}
+		};
+	protected:
+		/// Container type for terms.
+		typedef hop_table<term_type,term_hasher> container_type;
+	private:
+		// Overload for completely different term type: convert to term_type and proceed.
+		template <bool Sign, typename T, typename EchelonDescriptor>
+		void dispatch_insertion(T &&term, EchelonDescriptor &ed, typename std::enable_if<
+			!std::is_same<typename strip_cv_ref<T>::type,term_type>::value
+			>::type * = piranha_nullptr)
+		{
+std::cout << "converting to term type\n";
+			dispatch_insertion<Sign>(term_type(std::forward<T>(term)),ed);
+		}
+		// Overload for term_type.
+		template <bool Sign, typename T, typename EchelonDescriptor>
+		void dispatch_insertion(T &&term, EchelonDescriptor &ed, typename std::enable_if<
+			std::is_same<typename strip_cv_ref<T>::type,term_type>::value
+			>::type * = piranha_nullptr)
+		{
+std::cout << "term type insert!\n";
+			// Debug checks.
+			piranha_assert(empty() || m_container.begin()->is_compatible(ed));
+			// Generate error if term is not compatible.
+			if (unlikely(!term.is_compatible(ed))) {
+				piranha_throw(std::invalid_argument,"cannot insert incompatible term");
+			}
+			// Discard ignorable term.
+			if (unlikely(term.is_ignorable(ed))) {
+				return;
+			}
+			insertion_impl<Sign>(std::forward<T>(term),ed);
+		}
+		// Insert compatible, non-ignorable term.
+		template <bool Sign, typename T, typename EchelonDescriptor>
+		void insertion_impl(T &&term, EchelonDescriptor &ed, typename std::enable_if<
+			std::is_same<typename strip_cv_ref<T>::type,term_type>::value
+			>::type * = piranha_nullptr)
+		{
+			// Try to locate the term.
+			const auto it = m_container.find(term);
+			if (it == m_container.end()) {
+std::cout << "new term!\n";
+				// This is a new term, insert it.
+				const auto result = m_container.insert(std::forward<T>(term));
+				piranha_assert(result.second);
+				// Change sign if requested.
+				if (!Sign) {
+std::cout << "negating!\n";
+					result.first->m_cf.negate(ed);
+				}
+			} else {
+std::cout << "updating existing term!\n";
+				// Assert the existing term is not ignorable.
+				piranha_assert(!it->is_ignorable(ed));
+				// The term exists already, update it.
+				if (Sign) {
+					// TODO: move semantics here? Could be useful for coefficient series.
+std::cout << "adding!\n";
+					it->m_cf.add(term.m_cf,ed);
+				} else {
+std::cout << "subtracting!\n";
+					it->m_cf.subtract(term.m_cf,ed);
+				}
+				// Check if the term has become ignorable after the modification.
+				if (unlikely(it->is_ignorable(ed))) {
+std::cout << "erasing term become ignorable!\n";
+					m_container.erase(it);
+				}
+			}
+		}
+	public:
+		/// Size type.
+		/**
+		 * Used to represent the number of terms in the series. Equivalent to piranha::hop_table::size_type.
+		 */
+		typedef typename container_type::size_type size_type;
+		/// Defaulted default constructor.
+		base_series() = default;
+		/// Defaulted copy constructor.
+		base_series(const base_series &) = default;
+		/// Defaulted move constructor.
+		base_series(base_series &&) = default;
+		/// Defaulted destructor.
+		~base_series() = default;
+		/// Defaulted copy-assignment operator.
+		base_series &operator=(const base_series &) = default;
+		/// Move assignment operator.
+		/**
+		 * @param[in] other assignment argument.
+		 * 
+		 * @return reference to \p this.
+		 */
+		base_series &operator=(base_series &&other) piranha_noexcept_spec(true)
+		{
+			m_container = std::move(other.m_container);
+			return *this;
+		}
+		/// Series size.
+		/**
+		 * @return the number of terms in the series.
+		 */
+		size_type size() const
+		{
+			return m_container.size();
+		}
+		/// Empty test.
+		/**
+		 * @return \p true if size() is nonzero, \p false otherwise.
+		 */
+		bool empty() const
+		{
+			return !size();
+		}
+		/// Overload stream operator for piranha::base_series.
+		/**
+		 * Will direct to stream a human-readable representation of the series.
+		 * 
+		 * @param[in,out] os target stream.
+		 * @param[in] bs piranha::base_series argument.
+		 * 
+		 * @return reference to \p os.
+		 * 
+		 * @throws unspecified any exception resulting from directing to stream instances of piranha::base_series::term_type.
+		 */
+		friend inline std::ostream &operator<<(std::ostream &os, const base_series &bs)
+		{
+			for (auto it = bs.m_container.begin(); it != bs.m_container.end(); ++it) {
+				os << *it << '\n';
+			}
+			return os;
+		}
+	protected:
+		/// Insert generic term.
+		/**
+		 * This template method is activated only if \p T derives from piranha::base_term, and will insert \p term into the series
+		 * using \p ed as reference piranha::echelon_descriptor.
+		 * 
+		 * The insertions algorithm proceeds as follows:
+		 * 
+		 * - if \p term is not of type base_series::term_type, it is forwarded to construct an instance of
+		 *   base_series::term_type, and the algorithm proceeds to insert that instead;
+		 * - if the term is not compatible for insertion, a \p std::invalid_argument exception is thrown;
+		 * - if the term is ignorable, the method will return;
+		 * - if the term is already in the series, then:
+		 *   - its coefficient is added (if \p Sign is \p true) or subtracted (if \p Sign is \p false)
+		 *     to the existing term's coefficient;
+		 *   - if, after the addition/subtraction the existing term is ignorable, it will be erased;
+		 * - else:
+		 *   - the term is inserted into the term container and, if \p Sign is \p false, its coefficient is negated.
+		 * 
+		 * @param[in] term term to be inserted.
+		 * @param[in] ed reference piranha::echelon_descriptor.
+		 * 
+		 * @throws unspecified any exception thrown by:
+		 * - the constructor of base_series::term_type from type \p T;
+		 * - the \p is_compatible and \p is_ignorable methods of base_series::term_type;
+		 * - piranha::hop_table::insert().
+		 * @throws std::invalid_argument if \p term is ignorable.
+		 */
+		template <bool Sign, typename T, typename Term>
+		void insert(T &&term, const echelon_descriptor<Term> &ed, typename std::enable_if<
+			std::is_base_of<detail::base_term_tag,typename strip_cv_ref<T>::type>::value
+			>::type * = piranha_nullptr)
+		{
+			dispatch_insertion<Sign>(std::forward<T>(term),ed);
+		}
+		/// Insert generic term with <tt>Sign = true</tt>.
+		/**
+		 * Convenience wrapper for the generic insert() method, with \p Sign set to \p true.
+		 * 
+		 * @param[in] term term to be inserted.
+		 * @param[in] ed reference piranha::echelon_descriptor.
+		 * 
+		 * @throws unspecified any exception thrown by generic insert().
+		 */
+		template <typename T, typename EchelonDescriptor>
+		void insert(T &&term, EchelonDescriptor &ed, typename std::enable_if<
+			std::is_base_of<detail::base_term_tag,typename strip_cv_ref<T>::type>::value
+			>::type * = piranha_nullptr)
+		{
+			insert<true>(std::forward<T>(term),ed);
+		}
+	protected:
+		/// Terms container.
+		container_type m_container;
+};
+
+}
+
+#endif
---
+++ src/echelon_descriptor.hpp
@@ -0,0 +1,332 @@
+/***************************************************************************
+ *   Copyright (C) 2009-2011 by Francesco Biscani                          *
+ *   bluescarni@gmail.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef PIRANHA_ECHELON_DESCRIPTOR_HPP
+#define PIRANHA_ECHELON_DESCRIPTOR_HPP
+
+#include <algorithm>
+#include <boost/concept/assert.hpp>
+#include <boost/integer_traits.hpp>
+#include <boost/iterator/transform_iterator.hpp>
+#include <cstddef>
+#include <deque>
+#include <iterator>
+#include <stdexcept>
+#include <tuple>
+#include <type_traits>
+#include <vector>
+
+#include "concepts/term.hpp"
+#include "config.hpp"
+#include "detail/base_term_fwd.hpp"
+#include "detail/echelon_descriptor_fwd.hpp"
+#include "exceptions.hpp"
+#include "symbol.hpp"
+#include "type_traits.hpp"
+
+namespace piranha
+{
+
+/// Echelon descriptor.
+/**
+ * This class describes the echelon structure of a term of type \p TopLevelTerm by means of 
+ * an arguments tuple containing, for each level of the hierarchy, a vector of piranha::symbol instances representing the symbolic
+ * arguments in that echelon level. The vectors in the arguments tuple are always kept sorted alphabetically, and no duplicate
+ * symbols are allowed in the same echelon level. Getters method to
+ * access and manipulate the arguments tuple are provided.
+ * 
+ * \section type_requirements Type requirements
+ * 
+ * \p TopLevelTerm must be a model of piranha::concept::Term.
+ * 
+ * \section exception_safety Exception safety guarantees
+ * 
+ * This class offers the strong exception safety guarantee for all operations.
+ * 
+ * \section move_semantics Move semantics
+ * 
+ * Move construction and move assignment will leave the moved-from object in the same state of a moved-from \p std::tuple of \p std::vector.
+ * 
+ * @author Francesco Biscani (bluescarni@gmail.com)
+ */
+template <typename TopLevelTerm>
+class echelon_descriptor
+{
+		BOOST_CONCEPT_ASSERT((concept::Term<TopLevelTerm>));
+		// Template MP to define a tuple of uniform types.
+		template <typename T, std::size_t Size>
+		struct ntuple
+		{
+			static_assert(Size > 0,"Size must be strictly positive.");
+			typedef decltype(std::tuple_cat(std::tuple<T>{},typename ntuple<T,Size - static_cast<std::size_t>(1)>::type{})) type;
+		};
+		template <typename T>
+		struct ntuple<T,static_cast<std::size_t>(1)>
+		{
+			typedef std::tuple<T> type;
+		};
+	public:
+		/// Alias for \p TopLevelTerm.
+		typedef TopLevelTerm top_level_term_type;
+		/// Tuple of vectors of piranha::symbol.
+		typedef typename ntuple<std::vector<symbol>,echelon_size<top_level_term_type>::value>::type args_tuple_type;
+		/// Tuple for the representation of differences between two echelon descriptors.
+		/**
+		 * Each element of the tuple
+		 * is a vector of vectors of indices, which, for each echelon level, represents how symbols from one descriptor must
+		 * be inserted into the other in order to merge the two descriptors.
+		 * 
+		 * @see echelon_descriptor::difference() for an explanation of how this type is constructed and used.
+		 */
+		typedef typename ntuple<std::vector<std::vector<std::vector<symbol>::size_type>>,
+			echelon_size<top_level_term_type>::value>::type diff_tuple_type;
+	private:
+		static std::vector<std::vector<std::vector<symbol>::size_type>>
+			get_symbol_diff(const std::vector<symbol> &v1, const std::vector<symbol> &v2)
+		{
+			// Functor to locate the index of a symbol in v2.
+			struct f_type
+			{
+				typedef std::vector<symbol>::size_type result_type;
+				f_type(const std::vector<symbol> &v2):m_v2(v2) {}
+				result_type operator()(const symbol &s) const
+				{
+					result_type retval = 0;
+					for (; retval < m_v2.size(); ++retval) {
+						if (m_v2[retval] == s) {
+							break;
+						}
+					}
+					piranha_assert(retval != m_v2.size());
+					return retval;
+				}
+				const std::vector<symbol> &m_v2;
+			};
+			f_type f{v2};
+			std::vector<std::vector<std::vector<symbol>::size_type>> retval;
+			// Find the difference between the two sets of symbols: all elements in v2 that are not in v1.
+			std::deque<symbol> difference;
+			std::set_difference(v2.begin(),v2.end(),v1.begin(),v1.end(),std::back_insert_iterator<std::deque<symbol>>{difference});
+			// Build the return value.
+			for (decltype(v1.size()) index = 0; index < v1.size(); ++index) {
+				retval.push_back(std::vector<std::vector<symbol>::size_type>{});
+				while (difference.size() && !(v1[index] < difference.front())) {
+					retval.back().push_back(f(difference.front()));
+					difference.pop_front();
+				}
+			}
+			// Push back at the end any remaining symbol in the difference.
+			typedef boost::transform_iterator<decltype(f),decltype(difference.begin())> t_iter;
+			retval.push_back(std::vector<std::vector<symbol>::size_type>{});
+			retval.back().insert(retval.back().end(),t_iter(difference.begin(),f),t_iter(difference.end(),f));
+			return retval;
+		}
+		template <std::size_t N = 0, typename Enable = void>
+		struct difference_impl
+		{
+			static typename ntuple<std::vector<std::vector<std::vector<symbol>::size_type>>,echelon_size<top_level_term_type>::value - N>::type
+				run(const args_tuple_type &args_tuple1, const args_tuple_type &args_tuple2)
+			{
+				return std::tuple_cat(std::make_tuple(get_symbol_diff(std::get<N>(args_tuple1),std::get<N>(args_tuple2))),
+					difference_impl<N + static_cast<std::size_t>(1)>::run(args_tuple1,args_tuple2));
+			}
+		};
+		template <std::size_t N>
+		struct difference_impl<N,typename std::enable_if<N == echelon_size<TopLevelTerm>::value - static_cast<std::size_t>(1)>::type>
+		{
+			static std::tuple<std::vector<std::vector<std::vector<symbol>::size_type>>>
+				run(const args_tuple_type &args_tuple1, const args_tuple_type &args_tuple2)
+			{
+				return std::make_tuple(get_symbol_diff(
+					std::get<echelon_size<TopLevelTerm>::value - static_cast<std::size_t>(1)>(args_tuple1),
+					std::get<echelon_size<TopLevelTerm>::value - static_cast<std::size_t>(1)>(args_tuple2))
+				);
+			}
+		};
+		template <std::size_t N = 0, typename Enable = void>
+		struct sort_check_impl
+		{
+			static bool run(const args_tuple_type &args_tuple)
+			{
+				if (!std::is_sorted(std::get<N>(args_tuple).begin(),std::get<N>(args_tuple).end())) {
+					return false;
+				} else {
+					return sort_check_impl<N + static_cast<std::size_t>(1)>::run(args_tuple);
+				}
+			}
+		};
+		template <std::size_t N>
+		struct sort_check_impl<N,typename std::enable_if<N == echelon_size<TopLevelTerm>::value - static_cast<std::size_t>(1)>::type>
+		{
+			static bool run(const args_tuple_type &args_tuple)
+			{
+				return std::is_sorted(
+					std::get<echelon_size<TopLevelTerm>::value - static_cast<std::size_t>(1)>(args_tuple).begin(),
+					std::get<echelon_size<TopLevelTerm>::value - static_cast<std::size_t>(1)>(args_tuple).end()
+				);
+			}
+		};
+	public:
+		/// Defaulted default constructor.
+		echelon_descriptor() = default;
+		/// Defaulted copy constructor.
+		/**
+		 * @throw unspecified any exception thrown by the copy constructor of \p std::vector of piranha::symbol.
+		 */
+		echelon_descriptor(const echelon_descriptor &) = default;
+		/// Move constructor.
+		/**
+		 * @param[in] other descriptor to move from.
+		 */
+		echelon_descriptor(echelon_descriptor &&other) piranha_noexcept_spec(true) : m_args_tuple(std::move(other.m_args_tuple)) {}
+		/// Destructor.
+		/**
+		 * Equivalent to the default destructor, apart from checks being performed in debug mode.
+		 */
+		~echelon_descriptor() piranha_noexcept_spec(true)
+		{
+			piranha_assert(destruction_checks());
+		}
+		/// Copy assignment operator.
+		/**
+		 * @param[in] other descriptor to be copied.
+		 * 
+		 * @return reference to \p this.
+		 * 
+		 * @throws unspecified any exception thrown by the copy constructor of \p std::vector of piranha::symbol.
+		 */
+		echelon_descriptor &operator=(const echelon_descriptor &other)
+		{
+			// Use copy-and-move to make it exception-safe.
+			if (this != &other) {
+				auto tmp(other);
+				*this = std::move(other);
+			}
+			return *this;
+		}
+		/// Move assignment operator.
+		/**
+		 * Will move the members of \p other into \p this.
+		 * 
+		 * @param[in] other assignment argument.
+		 * 
+		 * @return reference to \p this.
+		 */
+		echelon_descriptor &operator=(echelon_descriptor &&other) piranha_noexcept_spec(true)
+		{
+			m_args_tuple = std::move(other.m_args_tuple);
+			return *this;
+		}
+		/// Arguments getter.
+		/**
+		 * @return const reference to the arguments vector at the echelon level corresponding
+		 * to type \p Term. If \p Term does not appear in the echelon hierarchy of echelon_descriptor::top_level_term_type,
+		 * a compile-time error will be produced.
+		 */
+		template <typename Term>
+		const std::vector<symbol> &get_args() const
+		{
+			return std::get<echelon_position<top_level_term_type,Term>::value>(m_args_tuple);
+		}
+		/// Arguments tuple getter.
+		/**
+		 * @return const reference to the arguments tuple.
+		 */
+		const args_tuple_type &get_args_tuple() const
+		{
+			return m_args_tuple;
+		}
+		/// Calculate difference between echelon descriptors.
+		/**
+		 * The difference between two echelon descriptors is described for each level of the echelon hierarchy in terms of the
+		 * differences between the corresponding arguments vectors. Given two argument vectors \p a1 and \p a2, the difference of \p a1
+		 * with respect to \p a2 is given as a structure describing how elements of \p a2 not appearing in \p a1 must be inserted into \p a1
+		 * so that, after such merge operation, \p a1 is an ordered set representing the union of the arguments in \p a1 and \p a2. This structure
+		 * is a vector \p d - whose size is <tt>a1</tt>'s size plus one - of vectors of indices. The element at index \p n in \p d contains
+		 * the indices of those elements in \p a2 which must be inserted into \p a1 before \p n in order to merge \p a1 and \p a2.
+		 * 
+		 * For instance, given the two vectors of arguments <tt>a1 = ['c','e']</tt> and <tt>a2 = ['a','b','c','f']</tt>, vector \p d will be
+		 * <tt>d = [[0,1],[],[3]]</tt>. This means that arguments <tt>['a','b']</tt> must be inserted before the first position in
+		 * \p a1, that no arguments must be inserted before <tt>'e'</tt> in \p a1 and that argument <tt>'f'</tt> must be inserted after the end of \p a1.
+		 * 
+		 * If the echelon size of \p TopLevelTerm2 is different from that of \p TopLevelTerm, a compile-time error will be produced.
+		 * 
+		 * @param[in] other piranha::echelon_descriptor with respect to which the difference will be calculated.
+		 * 
+		 * @return tuple of difference vectors.
+		 * 
+		 * @throws unspecified any exception thrown in case of memory allocation errors by standard containers.
+		 */
+		template <typename TopLevelTerm2>
+		diff_tuple_type difference(const echelon_descriptor<TopLevelTerm2> &other) const
+		{
+			static_assert(echelon_size<TopLevelTerm>::value == echelon_size<TopLevelTerm2>::value,"Incompatible echelon sizes.");
+			return difference_impl<>::run(m_args_tuple,other.m_args_tuple);
+		}
+		/// Add symbol.
+		/**
+		 * The piranha::symbol \p s will be inserted into the arguments vector at the echelon position corresponding to \p Term.
+		 * \p s must not be already present in the echelon position.
+		 * 
+		 * If \p Term does not appear in the echelon hierarchy of echelon_descriptor::top_level_term_type,
+		 * a compile-time error will be produced.
+		 * 
+		 * @param[in] s piranha::symbol to be inserted into the descriptor.
+		 * 
+		 * @throws unspecified any exception thrown in case of memory allocation errors by standard containers.
+		 * @throws std::invalid_argument if the symbol being added is already present in the destination echelon position.
+		 */
+		template <typename Term>
+		void add_symbol(const symbol &s)
+		{
+			auto &args_vector = std::get<echelon_position<top_level_term_type,Term>::value>(m_args_tuple);
+			auto new_args_vector = args_vector;
+			const auto it = std::lower_bound(new_args_vector.begin(),new_args_vector.end(),s);
+			if (unlikely(it != new_args_vector.end() && *it == s)) {
+				piranha_throw(std::invalid_argument,"symbol already present in this echelon level");
+			}
+			auto it_to_index = [](typename std::remove_const<decltype(it)>::type begin, typename std::remove_const<decltype(it)>::type target) -> std::size_t {
+				std::size_t retval = 0;
+				while (begin != target) {
+					if (unlikely(retval == boost::integer_traits<std::size_t>::const_max)) {
+						piranha_throw(std::overflow_error,"overflow error");
+					}
+					++retval;
+					++begin;
+				}
+				return retval;
+			};
+			const auto index = it_to_index(new_args_vector.begin(),it);
+			new_args_vector.insert(it,s);
+			// Move in the new args vector.
+			args_vector = std::move(new_args_vector);
+		}
+	private:
+		bool destruction_checks() const
+		{
+			return sort_check_impl<>::run(m_args_tuple);
+		}
+	private:
+		args_tuple_type m_args_tuple;
+};
+
+}
+
+#endif
