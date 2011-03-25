8836cdbe24276669584b65251d51e34650f26518
Francesco Biscani <bluescarni@gmail.com>
2011-03-25 13:07:35 +0100

    Added file for monomial exponent concept.

---
+++ src/concepts/monomial_exponent.hpp
@@ -0,0 +1,57 @@
+/***************************************************************************
+ *   Copyright (C) 2009-2011 by Francesco Biscani                          *
+ *   bluescarni@gmail.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef PIRANHA_CONCEPT_MONOMIAL_EXPONENT_HPP
+#define PIRANHA_CONCEPT_MONOMIAL_EXPONENT_HPP
+
+#include <boost/concept_check.hpp>
+
+#include "container_element.hpp"
+
+namespace piranha
+{
+
+namespace concept
+{
+
+/// Concept for types suitable for use as exponents in piranha::monomial.
+/**
+ * The requisites for type \p T are the following:
+ * 
+ * - must be a model of piranha::concept::ContainerElement,
+ * - must be constructible from the integral constant 0.
+ */
+template <typename T>
+struct MonomialExponent:
+	ContainerElement<T>
+{
+	/// Concept usage pattern.
+	BOOST_CONCEPT_USAGE(MonomialExponent)
+	{
+		T inst = T(0);
+		// Shut off compiler warning.
+		(void)&inst;
+	}
+};
+
+}
+}
+
+#endif
