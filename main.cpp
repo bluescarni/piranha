9e1bb5be4da18eda3a40207bffd6f83235dcc215
Francesco Biscani <bluescarni@gmail.com>
2011-06-08 14:27:42 +0200

    Updated main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,176 +1,132 @@
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <iostream>
+#include <random>
+
 #include "src/piranha.hpp"
 
 using namespace piranha;
 
-template <typename Cf, typename Expo>
-class polynomial:
-	public top_level_series<polynomial_term<Cf,Expo>,polynomial<Cf,Expo>>
-{
-		typedef top_level_series<polynomial_term<Cf,Expo>,polynomial<Cf,Expo>> base;
-	public:
-		polynomial() = default;
-		polynomial(const polynomial &) = default;
-		polynomial(polynomial &&) = default;
-		explicit polynomial(const std::string &name):base()
-		{
-			typedef typename base::term_type term_type;
-			// Insert the symbol.
-			this->m_ed.template add_symbol<term_type>(symbol(name));
-			// Construct and insert the term.
-			this->insert(term_type(Cf(1,this->m_ed),typename term_type::key_type{Expo(1)}),this->m_ed);
-		}
-		~polynomial() = default;
-		polynomial &operator=(const polynomial &) = default;
-		polynomial &operator=(polynomial &&other) piranha_noexcept_spec(true)
-		{
-			base::operator=(std::move(other));
-			return *this;
-		}
-		template <typename T>
-		polynomial &operator=(T &&x)
-		{
-			base::operator=(std::forward<T>(x));
-			return *this;
-		}
-};
+// template <typename Base>
+// class base_complex: public Base
+// {
+// 	public:
+// 		base_complex() = default;
+// 		base_complex(const base_complex &) = default;
+// 		base_complex(base_complex &&) = default;
+// 		~base_complex() = default;
+// 		base_complex &operator=(const base_complex &) = default;
+// 		base_complex &operator=(base_complex &&other)
+// 		{
+// 			Base::operator=(std::move(other));
+// 			return *this;
+// 		}
+// 		template <typename T>
+// 		base_complex &operator=(T &&x)
+// 		{
+// std::cout << "complex assign!!!\n";
+// 			Base::operator=(std::forward<T>(x));
+// 			return *this;
+// 		}
+// 	protected:
+// 		template <typename Series1, typename Series2>
+// 		explicit base_complex(Series1 &&s1, Series2 &&s2, const echelon_descriptor<typename Base::term_type> &ed,
+// 			typename std::enable_if<std::is_base_of<detail::base_series_tag,typename strip_cv_ref<Series1>::type>::value &&
+// 			std::is_base_of<detail::base_series_tag,typename strip_cv_ref<Series2>::type>::value>::type * = piranha_nullptr)
+// 		{
+// std::cout << "LOLLER called!!!\n";
+// 		}
+// };
 
-template <typename Base>
-class base_complex: public Base
+void gogo()
 {
-	public:
-		base_complex() = default;
-		base_complex(const base_complex &) = default;
-		base_complex(base_complex &&) = default;
-		~base_complex() = default;
-		base_complex &operator=(const base_complex &) = default;
-		base_complex &operator=(base_complex &&other)
-		{
-			Base::operator=(std::move(other));
-			return *this;
-		}
-		template <typename T>
-		base_complex &operator=(T &&x)
-		{
-std::cout << "complex assign!!!\n";
-			Base::operator=(std::forward<T>(x));
-			return *this;
-		}
-	protected:
-		template <typename Series1, typename Series2>
-		explicit base_complex(Series1 &&s1, Series2 &&s2, const echelon_descriptor<typename Base::term_type> &ed,
-			typename std::enable_if<std::is_base_of<detail::base_series_tag,typename strip_cv_ref<Series1>::type>::value &&
-			std::is_base_of<detail::base_series_tag,typename strip_cv_ref<Series2>::type>::value>::type * = piranha_nullptr)
-		{
-std::cout << "LOLLER called!!!\n";
-		}
-};
+// 	thread_management::binder b;
+	typedef polynomial<numerical_coefficient<double>,int16_t> p_type;
+	p_type x("x"), y("y"), z("z"), t("t");
+	auto f = 1 + x + y + z + t, tmp(f);
+	for (int i = 1; i < 10; ++i) {
+		f *= tmp;
+	}
+	std::cout << f.size() << '\n';
+	auto g = f + 1;
+	const boost::posix_time::ptime time0 = boost::posix_time::microsec_clock::local_time();
+	auto retval = f * g;
+	std::cout << "Elapsed time: " << (double)(boost::posix_time::microsec_clock::local_time() - time0).total_microseconds() / 1000 << '\n';
+	std::cout << retval.size() << '\n';
+}
 
-template <typename Cf, typename Expo>
-class polynomial2:
-	public base_complex<top_level_series<polynomial_term<Cf,Expo>,polynomial2<Cf,Expo>>>
+void blo()
 {
-		typedef base_complex<top_level_series<polynomial_term<Cf,Expo>,polynomial2<Cf,Expo>>> base;
-	public:
-		polynomial2() = default;
-		polynomial2(const polynomial2 &) = default;
-		polynomial2(polynomial2 &&) = default;
-		explicit polynomial2(const std::string &name):base()
-		{
-			typedef typename base::term_type term_type;
-			// Insert the symbol.
-			this->m_ed.template add_symbol<term_type>(symbol(name));
-			// Construct and insert the term.
-			this->insert(term_type(Cf(1,this->m_ed),typename term_type::key_type{Expo(1)}),this->m_ed);
-		}
-		explicit polynomial2(const char *name):base()
-		{
-			typedef typename base::term_type term_type;
-			// Insert the symbol.
-			this->m_ed.template add_symbol<term_type>(symbol(name));
-			// Construct and insert the term.
-			this->insert(term_type(Cf(1,this->m_ed),typename term_type::key_type{Expo(1)}),this->m_ed);
-		}
-		template <typename... Args>
-		explicit polynomial2(Args && ... params):base(std::forward<Args>(params)...) {}
-		~polynomial2() = default;
-		polynomial2 &operator=(const polynomial2 &) = default;
-		polynomial2 &operator=(polynomial2 &&other) piranha_noexcept_spec(true)
-		{
-			base::operator=(std::move(other));
-			return *this;
-		}
-		template <typename T>
-		polynomial2 &operator=(T &&x)
-		{
-std::cout << "poly2 assign\n";
-			base::operator=(std::forward<T>(x));
-			return *this;
-		}
-};
-
-struct foo
-{
-	foo() = default;
-	foo(foo &&) = delete;
-	foo(const foo &) = default;
-};
+	typedef polynomial<numerical_coefficient<double>,int16_t> p_type;
+	p_type x("x"), y("y"), z("z"), t("t"), u("u");
+	auto f = (x + y + z*z*2 + t*t*t*3 + u*u*u*u*u*5 + 1), tmp_f(f);
+	auto g = (u + t + z*z*2 + y*y*y*3 + x*x*x*x*x*5 + 1), tmp_g(g);
+	for (int i = 1; i < 12; ++i) {
+		f *= tmp_f;
+		g *= tmp_g;
+	}
+	std::cout << f.size() << '\n';
+	std::cout << g.size() << '\n';
+	const boost::posix_time::ptime time0 = boost::posix_time::microsec_clock::local_time();
+	f *= g;
+	std::cout << "Elapsed time: " << (double)(boost::posix_time::microsec_clock::local_time() - time0).total_microseconds() / 1000 << '\n';
+	std::cout << f.size() << '\n';
+}
 
 int main()
 {
-// 	std::cout << is_nothrow_move_assignable<int &>::value << '\n';
-	typedef polynomial2<numerical_coefficient<integer>,int> p_type;
-	typedef polynomial2<numerical_coefficient<integer>,int> p2_type;
-	typedef polynomial2<numerical_coefficient<double>,int> p3_type;
+// 	blo();
+// 	return 0;
+	
+	typedef polynomial<numerical_coefficient<double>,int32_t> p_type;
+	typedef p_type::term_type term_type;
+	typedef term_type::cf_type cf_type;
+	typedef term_type::key_type key_type;
+	std::uniform_real_distribution<double> rd(-99.,99.);
+	std::minstd_rand re;
+	std::uniform_int_distribution<int32_t> id(1,2500);
+	std::mt19937 ie;
+	p_type f(rd(re)), x("x");
+	f += x;
+	f -= x;
+	int cur_exp = 0;
+	for (auto i = 1; i < 8192; ++i) {
+		cur_exp += id(ie);
+		f.insert(term_type(cf_type(rd(re),f.m_ed),key_type({cur_exp})),f.m_ed);
+	}
+	auto input_size = f.size();
+	f *= f;
+	std::cout << "final = " << f.size() << '\n';
+	std::cout << "W = " << (((double)input_size * input_size) / f.size()) << '\n';
+// 	thread_group tg;
+// 	tg.create_thread(gogo);
+// 	tg.create_thread(blo);
+// 	tg.create_thread(gogo);
+// 	tg.join_all();
+// 	gogo();
+// 	return 0;
 
-	p2_type p, q;
-	p = 5;
-	p = q;
+// 	std::unordered_set<integer> hs(12000000);
+// 	for (int i = 0; i < 6000000; ++i) {
+// 		hs.insert(integer(-i * i));
+// 	}
+// 	std::cout << hs.size() << '\n';
 
-	std::cout << (p == 0) << '\n';
-	std::cout << (0 == p) << '\n';
-	
-	p = 5;
+// 	typedef polynomial<numerical_coefficient<double>,int16_t> p_type;
+// 	p_type x("x"), y("y"), z("z"), t("t");
+// 	auto f = 1 + x + y + z + t, tmp(f);
+// 	for (int i = 1; i < 20; ++i) {
+// 		f *= tmp;
+// 	}
+// 	std::cout << f.size() << '\n';
+// 	const boost::posix_time::ptime time0 = boost::posix_time::microsec_clock::local_time();
+// 	auto retval = f * (f + 1);
+// 	std::cout << "Elapsed time: " << (double)(boost::posix_time::microsec_clock::local_time() - time0).total_microseconds() / 1000 << '\n';
+// 	std::cout << retval.size() << '\n';
+// 	return 0;
 
-	std::cout << (p == 5) << '\n';
-	std::cout << (5 == p) << '\n';
-	std::cout << (q == p) << '\n';
-	
-	q = 5;
-// 	std::cout << (q == p) << '\n';
-// 	
-// 	std::cout << (q == p_type{}) << '\n';
-// 	std::cout << (q == p3_type{}) << '\n';
-	std::cout << (p3_type{} == q) << '\n';
-	
-	std::cout << (p3_type{"a"} == p2_type{}) << '\n';
-	std::cout << (p3_type{} == p2_type{"a"}) << '\n';
-	std::cout << (p2_type{"a"} == p3_type{"a"}) << '\n';
-	std::cout << (p3_type{"a"} == p2_type{"a"}) << '\n';
-// 	foo f = foo{};
-	return 0;
-	
-	
-	
-	polynomial<numerical_coefficient<double>,int> x("x");
-	std::cout << x << '\n';
-	x += 4;
-	std::cout << x << '\n';
-	x += integer(5);
-	std::cout << x << '\n';
-	x += x;
-	polynomial2<numerical_coefficient<integer>,int> x2("x");
-	x += std::move(x2);
-	polynomial2<numerical_coefficient<integer>,int> x3("x");
-	x += std::move(x3);
-
-	polynomial2<numerical_coefficient<integer>,int> y("y");
-	x += y;
- 	x + y;
-	std::cout << x << "\n-------------\n";
-	std::cout << x + y << "\n-------------\n";
-// 	2 + x;
-// 	std::cout << x << "\n-------------\n";
-	3 + polynomial2<numerical_coefficient<std::complex<double>>,int>{};
-// 	p2_type foo, bar(foo);
-// 	p2_type frob{foo,bar,echelon_descriptor<p2_type::term_type>{}};
+// 	std::unordered_set<integer> ht;
+// 	for (int i = 0; i < 200000; ++i) {
+// 		integer tmp(i);
+// 		ht.insert(tmp);
+// 	}
 }
