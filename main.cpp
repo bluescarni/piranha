73e2a5c4aebe9308b49cc4bb005d43efbfa787cf
Francesco Biscani <bluescarni@gmail.com>
2011-06-10 16:48:46 +0200

    WIP updated main.cpp.

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@ void gogo()
 	typedef polynomial<numerical_coefficient<double>,int16_t> p_type;
 	p_type x("x"), y("y"), z("z"), t("t");
 	auto f = 1 + x + y + z + t, tmp(f);
-	for (int i = 1; i < 10; ++i) {
+	for (int i = 1; i < 20; ++i) {
 		f *= tmp;
 	}
 	std::cout << f.size() << '\n';
@@ -56,26 +56,32 @@ void gogo()
 
 void blo()
 {
+	thread_management::binder b;
 	typedef polynomial<numerical_coefficient<double>,int16_t> p_type;
 	p_type x("x"), y("y"), z("z"), t("t"), u("u");
 	auto f = (x + y + z*z*2 + t*t*t*3 + u*u*u*u*u*5 + 1), tmp_f(f);
 	auto g = (u + t + z*z*2 + y*y*y*3 + x*x*x*x*x*5 + 1), tmp_g(g);
+	auto h = (x + y + u*u*2 + t*t*t*3 + z*z*z*z*z*5 + 1), tmp_h(h);
 	for (int i = 1; i < 12; ++i) {
 		f *= tmp_f;
 		g *= tmp_g;
+		h *= tmp_h;
 	}
 	std::cout << f.size() << '\n';
 	std::cout << g.size() << '\n';
-	const boost::posix_time::ptime time0 = boost::posix_time::microsec_clock::local_time();
 	f *= g;
+	g *= h;
+	const boost::posix_time::ptime time0 = boost::posix_time::microsec_clock::local_time();
+	f += std::move(g);
 	std::cout << "Elapsed time: " << (double)(boost::posix_time::microsec_clock::local_time() - time0).total_microseconds() / 1000 << '\n';
 	std::cout << f.size() << '\n';
 }
 
 int main()
 {
-// 	blo();
-// 	return 0;
+	settings::set_n_threads(1);
+	gogo();
+	return 0;
 	
 	typedef polynomial<numerical_coefficient<double>,int32_t> p_type;
 	typedef p_type::term_type term_type;
@@ -83,20 +89,28 @@ int main()
 	typedef term_type::key_type key_type;
 	std::uniform_real_distribution<double> rd(-99.,99.);
 	std::minstd_rand re;
-	std::uniform_int_distribution<int32_t> id(1,2500);
 	std::mt19937 ie;
-	p_type f(rd(re)), x("x");
-	f += x;
-	f -= x;
-	int cur_exp = 0;
-	for (auto i = 1; i < 8192; ++i) {
-		cur_exp += id(ie);
-		f.insert(term_type(cf_type(rd(re),f.m_ed),key_type({cur_exp})),f.m_ed);
+	for (int n = 0; n < 1250; n += 20) {
+		std::uniform_int_distribution<int32_t> id(1,10 + n);
+		p_type f(rd(re)), g(rd(re)), x("x");
+		f += x;
+		f -= x;
+		g += x;
+		g -= x;
+		int cur_exp1 = 0, cur_exp2 = 0;
+		for (auto i = 1; i < 8192; ++i) {
+			cur_exp1 += id(ie);
+			f.insert(term_type(cf_type(rd(re),f.m_ed),key_type({cur_exp1})),f.m_ed);
+			cur_exp2 += id(ie);
+			g.insert(term_type(cf_type(rd(re),g.m_ed),key_type({cur_exp2})),g.m_ed);
+		}
+		std::cout << "cur_exp1 " << cur_exp1 << '\n';
+		std::cout << "cur_exp2 " << cur_exp2 << '\n';
+		auto input_size = f.size();
+		f *= g;
+		std::cout << "final = " << f.size() << '\n';
+		std::cout << "W = " << (((double)input_size * input_size) / f.size()) << '\n';
 	}
-	auto input_size = f.size();
-	f *= f;
-	std::cout << "final = " << f.size() << '\n';
-	std::cout << "W = " << (((double)input_size * input_size) / f.size()) << '\n';
 // 	thread_group tg;
 // 	tg.create_thread(gogo);
 // 	tg.create_thread(blo);
