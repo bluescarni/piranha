25829771a1c65879eb5927815e818d5fd93f4106
Francesco Biscani <bluescarni@gmail.com>
2011-09-23 03:13:43 +0200

    echelon_size: add tests with coefficient series.

--- tests/echelon_size.cpp
+++ tests/echelon_size.cpp
@@ -26,28 +26,51 @@
 #include "../src/base_term.hpp"
 #include "../src/config.hpp"
 #include "../src/monomial.hpp"
+#include "../src/series.hpp"
 
 using namespace piranha;
 
-// TODO: test with larger echelon sizes once we have coefficient series.
-
-class term_type1: public base_term<double,monomial<int>,term_type1>
+template <typename Cf>
+class g_term_type: public base_term<Cf,monomial<int>,g_term_type<Cf>>
 {
 	public:
-		term_type1() = default;
-		term_type1(const term_type1 &) = default;
-		term_type1(term_type1 &&) = default;
-		term_type1 &operator=(const term_type1 &) = default;
-		term_type1 &operator=(term_type1 &&other) piranha_noexcept_spec(true)
+		g_term_type() = default;
+		g_term_type(const g_term_type &) = default;
+		g_term_type(g_term_type &&) = default;
+		g_term_type &operator=(const g_term_type &) = default;
+		g_term_type &operator=(g_term_type &&other) piranha_noexcept_spec(true)
 		{
-			base_term<double,monomial<int>,term_type1>::operator=(std::move(other));
+			base_term<Cf,monomial<int>,g_term_type>::operator=(std::move(other));
 			return *this;
 		}
 		// Needed to satisfy concept checking.
-		explicit term_type1(const double &, const monomial<int> &) {}
+		explicit g_term_type(const Cf &, const monomial<int> &) {}
+};
+
+template <typename Term>
+class g_series_type: public series<Term,g_series_type<Term>>
+{
+	public:
+		typedef series<Term,g_series_type<Term>> base;
+		g_series_type() = default;
+		g_series_type(const g_series_type &) = default;
+		g_series_type(g_series_type &&) = default;
+		g_series_type &operator=(const g_series_type &) = default;
+		g_series_type &operator=(g_series_type &&other)
+		{
+			if (this != &other) {
+				base::operator=(std::move(other));
+			}
+			return *this;
+		}
 };
 
 BOOST_AUTO_TEST_CASE(type_traits_echelon_size)
 {
-	BOOST_CHECK_EQUAL(echelon_size<term_type1>::value,std::size_t(1));
+	BOOST_CHECK_EQUAL(echelon_size<g_term_type<double>>::value,std::size_t(1));
+	typedef g_series_type<g_term_type<double>> series_type1;
+	typedef g_series_type<g_term_type<series_type1>> series_type2;
+	BOOST_CHECK_EQUAL(echelon_size<typename series_type2::term_type>::value,std::size_t(2));
+	typedef g_series_type<g_term_type<series_type2>> series_type3;
+	BOOST_CHECK_EQUAL(echelon_size<typename series_type3::term_type>::value,std::size_t(3));
 }
