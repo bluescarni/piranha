29f476fedf00df01394eca3b3dea329468dcb18e
Francesco Biscani <bluescarni@gmail.com>
2011-10-29 21:08:13 +0200

    Missing file.

---
+++ src/detail/sfinae_types.hpp
@@ -0,0 +1,43 @@
+/***************************************************************************
+ *   Copyright (C) 2009-2011 by Francesco Biscani                          *
+ *   bluescarni@gmail.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef PIRANHA_SFINAE_TYPES_HPP
+#define PIRANHA_SFINAE_TYPES_HPP
+
+#include <array>
+
+namespace piranha
+{
+
+namespace detail
+{
+
+// Types for SFINAE-based type traits.
+struct sfinae_types
+{
+	typedef char yes;
+	typedef std::array<char,2u> no;
+};
+
+}
+
+}
+
+#endif
