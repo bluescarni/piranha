5d4280a8c8b9cd1fabbc247b97fe1d89c64756f4
Francesco Biscani <bluescarni@gmail.com>
2011-10-31 17:57:13 +0100

    Couple of fixes for the headers.

--- src/concepts.hpp
+++ src/concepts.hpp
@@ -39,10 +39,10 @@ namespace concept {}
 #include "concepts/coefficient.hpp"
 #include "concepts/container_element.hpp"
 #include "concepts/crtp.hpp"
+#include "concepts/degree_key.hpp"
 #include "concepts/key.hpp"
 #include "concepts/multipliable_coefficient.hpp"
 #include "concepts/multipliable_term.hpp"
-#include "concepts/power_series_term.hpp"
 #include "concepts/series.hpp"
 #include "concepts/term.hpp"
 
