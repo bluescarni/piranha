b604610679f84594bcc704b14577dd74fce631df
Francesco Biscani <bluescarni@gmail.com>
2011-11-29 22:11:09 +0100

    Another todo item added.

--- src/piranha.hpp
+++ src/piranha.hpp
@@ -59,6 +59,8 @@
  * \todo review use of numeric_cast: in some places we might be using it in such a way we expect errors if converting floating point to int, but this is not the case (from the doc)
  * \todo the tuning parameters should be tested and justified (e.g., when to go into mt mode, etc.).
  * \todo think about the startup report, it might end up being too noisy.
+ * \todo probably we should make overflow-safe all operations on keys that currently are not: multiplication, computation of degree (including in truncators), etc.
+ * and then optimize instead in the multipliers (by checking the ranges before performing the multiplication).
  */
 namespace piranha
 {
