c56663103cd98134997820c4fe6ac1586ad29e0b
Francesco Biscani <bluescarni@gmail.com>
2011-12-09 04:29:28 +0100

    Stricter check on the type of the result of term multiplication.

--- src/concepts/multipliable_term.hpp
+++ src/concepts/multipliable_term.hpp
@@ -23,6 +23,7 @@
 
 #include <boost/concept_check.hpp>
 #include <type_traits>
+#include <tuple>
 
 #include "../symbol_set.hpp"
 #include "../type_traits.hpp"
@@ -39,7 +40,7 @@ namespace concept
  * The requisites for type \p T are the following:
  * 
  * - must be a model of piranha::concept::Term,
- * - must be provided with a typedef \p multiplication_result_type which is either \p T itself or a tuple of types \p T,
+ * - must be provided with a typedef \p multiplication_result_type which is either \p T itself or a tuple of types \p T of nonzero size,
  *   used to represent the result of the multiplication with another term,
  * - must be provided with a const member function <tt>multiply()</tt> accepting as first parameter a mutable reference
  *   to an instance of type \p multiplication_result_type, as second parameter a const reference to another term instance and
@@ -53,6 +54,12 @@ class MultipliableTerm:
 {
 	private:
 		PIRANHA_DECLARE_HAS_TYPEDEF(multiplication_result_type);
+		static void check_tuple(const std::true_type &)
+		{
+			static_assert(std::tuple_size<typename T::multiplication_result_type>::value > 0u,
+				"Invalid tuple length for term multiplication result type.");
+		}
+		static void check_tuple(const std::false_type &) {}
 	public:
 		/// Concept usage pattern.
 		BOOST_CONCEPT_USAGE(MultipliableTerm)
@@ -60,6 +67,7 @@ class MultipliableTerm:
 			static_assert(has_typedef_multiplication_result_type<T>::value,"Missing multiplication result type typedef.");
 			static_assert(std::is_same<typename T::multiplication_result_type,T>::value ||
 				is_tuple<typename T::multiplication_result_type>::value,"Invalid multiplication result type typedef.");
+			check_tuple(std::integral_constant<bool,is_tuple<typename T::multiplication_result_type>::value>());
 			typename T::multiplication_result_type retval;
 			typedef decltype(std::declval<T>().multiply(retval,std::declval<T>(),std::declval<symbol_set>())) ret_type;
 			static_assert(std::is_same<void,ret_type>::value,"Invalid return value type for multiply()");
