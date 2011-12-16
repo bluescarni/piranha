44ae1cc50f6159399fb75022f70cfcb58f9d2a9c
Francesco Biscani <bluescarni@gmail.com>
2011-12-16 02:02:56 +0100

    More minor doc.

--- src/piranha.hpp
+++ src/piranha.hpp
@@ -46,7 +46,8 @@
  * \todo concepts: how to deal with generic methods (e.g., coefficient in-place multiply by whatever)? We could add another parameter to the concept, with default void,
  * and use it explictly only when actually using that generic method? ADDENDUM: connected to this, the question of dealing with methods that
  * have additional type requirements wrt those delcared in class. Probably need to review all generic methods and add type requirements there.
- * \todo hashing of array_key based on kronecker substitution -> use static table of kronecker_array to encode the array key in an integer.
+ * \todo hashing of array_key based on kronecker substitution -> use static table of kronecker_array to encode the array key in an integer -> note that this becomes
+ * moot in case the homomorphic hashing thing works out.
  * \todo use cache aligning allocator by default in GMP memory allocation functions if in mt mode? investigate performance impact.
  * \todo assert that concepts are respected in models.
  * \todo check the include of pure C stuff (GMP is already fine, maybe the platform-specific stuff needs to be reviewed - aligning alloc, cache size, number of
