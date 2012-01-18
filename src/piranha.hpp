6aed40f3e719c5775bea28a236430ee905583f12
Francesco Biscani <bluescarni@gmail.com>
2012-01-18 11:22:15 +0100

    Add missing include.

--- src/piranha.hpp
+++ src/piranha.hpp
@@ -119,6 +119,7 @@ namespace detail {}
 #include "static_vector.hpp"
 #include "symbol.hpp"
 #include "symbol_set.hpp"
+#include "task_group.hpp"
 #include "thread_barrier.hpp"
 #include "thread_group.hpp"
 #include "thread_management.hpp"
