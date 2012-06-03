26c2b6f5ff61a5186dbbd0ae7ca34c4e897fe6df
Francesco Biscani <bluescarni@gmail.com>
2012-06-03 03:16:21 +0200

    Couple of minor build fixes.

--- pyranha/core.cpp
+++ pyranha/core.cpp
@@ -21,8 +21,8 @@
 // NOTE: the order of inclusion in the first two items here is forced by these two issues:
 // http://mail.python.org/pipermail/python-list/2004-March/907592.html
 // http://mail.python.org/pipermail/new-bugs-announce/2011-March/010395.html
-#include <Python.h>
 #include <cmath>
+#include <Python.h>
 #include <boost/python.hpp>
 #include <stdexcept>
 #include <string>
