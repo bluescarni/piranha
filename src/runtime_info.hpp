2acbd7793664ca4fe97b04bb346f11b2c19ff623
Francesco Biscani <bluescarni@gmail.com>
2013-09-21 02:35:35 +0200

    runtime_info: move everything to header.

--- src/runtime_info.hpp
+++ src/runtime_info.hpp
@@ -21,12 +21,64 @@
 #ifndef PIRANHA_RUNTIME_INFO_HPP
 #define PIRANHA_RUNTIME_INFO_HPP
 
+#if defined(__linux__)
+
+#include <boost/lexical_cast.hpp>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+extern "C"
+{
+#include <sys/sysinfo.h>
+#include <unistd.h>
+}
+
+#elif defined(__FreeBSD__)
+
+extern "C"
+{
+#include <sys/types.h>
+#include <sys/sysctl.h>
+}
+#include <cstddef>
+
+#elif defined(_WIN32)
+
+extern "C"
+{
+#include <Windows.h>
+}
+#include <cstddef>
+#include <cstdlib>
+#include <new>
+
+#endif
+
+#include <boost/numeric/conversion/cast.hpp>
+#include <memory>
 #include <thread>
 
 #include "config.hpp"
+#include "exceptions.hpp"
+#include "runtime_info.hpp"
 
 namespace piranha
 {
+
+namespace detail
+{
+
+template <typename = int>
+struct base_runtime_info
+{
+	static const std::thread::id m_main_thread_id;
+};
+
+template <typename T>
+const std::thread::id base_runtime_info<T>::m_main_thread_id = std::this_thread::get_id();
+
+}
 
 /// Runtime information.
 /**
@@ -34,8 +86,9 @@ namespace piranha
  * 
  * @author Francesco Biscani (bluescarni@gmail.com)
  */
-class PIRANHA_PUBLIC runtime_info
+class PIRANHA_PUBLIC runtime_info: private detail::base_runtime_info<>
 {
+		using base = detail::base_runtime_info<>;
 	public:
 		/// Main thread ID.
 		/**
@@ -45,10 +98,105 @@ class PIRANHA_PUBLIC runtime_info
 		{
 			return m_main_thread_id;
 		}
-		static unsigned get_cache_line_size();
-		static unsigned get_hardware_concurrency();
-	private:
-		static const std::thread::id	m_main_thread_id;
+		// NOTE: here we do not use boost's as we do not want to depend on Boost thread.
+		/// Hardware concurrency.
+		/**
+		 * @return number of detected hardware thread contexts (typically equal to the number of logical CPU cores), or 0 if
+		 * the detection fails.
+		 * 
+		 * @throws std::system_error in case of failure(s) by threading primitives.
+		 */
+		static unsigned get_hardware_concurrency()
+		{
+#if defined(__linux__)
+			int candidate = ::get_nprocs();
+			return (candidate <= 0) ? 0u : static_cast<unsigned>(candidate);
+#elif defined(__FreeBSD__)
+			int count;
+			std::size_t size = sizeof(count);
+			return ::sysctlbyname("hw.ncpu",&count,&size,NULL,0) ? 0u : static_cast<unsigned>(count);
+#elif defined(_WIN32)
+			::SYSTEM_INFO info = ::SYSTEM_INFO();
+			::GetSystemInfo(&info);
+			// info.dwNumberOfProcessors is a dword, i.e., a long unsigned integer.
+			// http://msdn.microsoft.com/en-us/library/cc230318(v=prot.10).aspx
+			unsigned retval = 0u;
+			try {
+				retval = boost::numeric_cast<unsigned>(info.dwNumberOfProcessors);
+			} catch (...) {
+				// Do not do anything, just keep zero.
+			}
+			return retval;
+#else
+			return 0u;
+#endif
+		}
+		/// Size of the data cache line.
+		/**
+		 * @return data cache line size (in bytes), or 0 if the value cannot be determined.
+		 * 
+		 * @throws std::system_error in case of failure(s) by threading primitives.
+		 */
+		static unsigned get_cache_line_size()
+		{
+#if defined(__linux__)
+			auto ls = ::sysconf(_SC_LEVEL1_DCACHE_LINESIZE);
+			// This can fail on some systems, resort to try reading the /sys entry.
+			// NOTE: here we could iterate over all cpus and take the maximum cache line size.
+			if (!ls) {
+				std::ifstream sys_file("/sys/devices/system/cpu/cpu0/cache/index0/coherency_line_size");
+				if (sys_file.is_open() && sys_file.good()) {
+					try {
+						std::string line;
+						std::getline(sys_file,line);
+						ls = boost::lexical_cast<decltype(ls)>(line);
+					} catch (...) {}
+				}
+			}
+			if (ls > 0) {
+				unsigned retval = 0u;
+				try {
+					retval = boost::numeric_cast<unsigned>(ls);
+				} catch (...) {}
+				return retval;
+			} else {
+				return 0u;
+			}
+#elif defined(_WIN32) && defined(PIRANHA_HAVE_SYSTEM_LOGICAL_PROCESSOR_INFORMATION)
+			// Adapted from:
+			// http://strupat.ca/2010/10/cross-platform-function-to-get-the-line-size-of-your-cache
+			std::size_t line_size = 0u;
+			::DWORD buffer_size = 0u;
+			::SYSTEM_LOGICAL_PROCESSOR_INFORMATION *buffer = 0;
+			// This is expected to fail, with a specific error code.
+			const auto retval = ::GetLogicalProcessorInformation(0,&buffer_size);
+			if (retval || ::GetLastError() != ERROR_INSUFFICIENT_BUFFER) {
+				return 0u;
+			}
+			try {
+				buffer = (::SYSTEM_LOGICAL_PROCESSOR_INFORMATION *)std::malloc(boost::numeric_cast<std::size_t>(buffer_size));
+				if (unlikely(!buffer)) {
+					piranha_throw(std::bad_alloc,);
+				}
+				if (!::GetLogicalProcessorInformation(&buffer[0u],&buffer_size)) {
+					std::free(buffer);
+					return 0u;
+				}
+				for (::DWORD i = 0u; i != buffer_size / sizeof(::SYSTEM_LOGICAL_PROCESSOR_INFORMATION); ++i) {
+					if (buffer[i].Relationship == ::_LOGICAL_PROCESSOR_RELATIONSHIP::RelationCache && buffer[i].Cache.Level == 1) {
+						line_size = buffer[i].Cache.LineSize;
+						break;
+					}
+				}
+				std::free(buffer);
+				return boost::numeric_cast<unsigned>(line_size);
+			} catch (...) {}
+			return 0u;
+#else
+			// TODO: FreeBSD, OSX, etc.?
+			return 0u;
+#endif
+		}
 };
 
 }
