d440367b214a450c7d0cbc62257e9276107a6b75
Francesco Biscani <bluescarni@gmail.com>
2014-05-24 13:43:23 +0200

    mp_integer: implement division.

--- src/mp_integer.hpp
+++ src/mp_integer.hpp
@@ -1402,6 +1402,74 @@ class mp_integer
 		{
 			return binary_mul(n,x);
 		}
+		// Division.
+		mp_integer &in_place_div(const mp_integer &other)
+		{
+			if (unlikely(other.sign() == 0)) {
+				piranha_throw(zero_division_error,"division by zero");
+			}
+			const bool s1 = is_static(), s2 = other.is_static();
+			if (s1 && s2) {
+				mp_integer r;
+				m_int.st.div(m_int.st,r.m_int.st,m_int.st,other.m_int.st);
+			} else if (s1 && !s2) {
+				// Promote this.
+				m_int.promote();
+				::mpz_tdiv_q(&m_int.dy,&m_int.dy,&other.m_int.dy);
+			} else if (!s1 && s2) {
+				// Create a promoted copy of other.
+				detail::mpz_raii m;
+				other.m_int.st.to_mpz(m.m_mpz);
+				::mpz_tdiv_q(&m_int.dy,&m_int.dy,&m.m_mpz);
+			} else {
+				::mpz_tdiv_q(&m_int.dy,&m_int.dy,&other.m_int.dy);
+			}
+			return *this;
+		}
+		template <typename T>
+		mp_integer &in_place_div(const T &other, typename std::enable_if<std::is_integral<T>::value>::type * = nullptr)
+		{
+			return in_place_div(mp_integer(other));
+		}
+		template <typename T>
+		mp_integer &in_place_div(const T &other, typename std::enable_if<std::is_floating_point<T>::value>::type * = nullptr)
+		{
+			return (*this = static_cast<T>(*this) / other);
+		}
+		template <typename T, typename U>
+		static mp_integer binary_div(const T &n1, const U &n2, typename std::enable_if<
+			std::is_same<T,mp_integer>::value && std::is_same<U,mp_integer>::value>::type * = nullptr)
+		{
+			mp_integer retval(n1);
+			retval /= n2;
+			return retval;
+		}
+		template <typename T, typename U>
+		static mp_integer binary_div(const T &n1, const U &n2, typename std::enable_if<
+			std::is_same<T,mp_integer>::value && std::is_integral<U>::value>::type * = nullptr)
+		{
+			mp_integer retval(n1);
+			retval /= n2;
+			return retval;
+		}
+		template <typename T, typename U>
+		static mp_integer binary_div(const T &n1, const U &n2, typename std::enable_if<
+			std::is_same<U,mp_integer>::value && std::is_integral<T>::value>::type * = nullptr)
+		{
+			mp_integer retval(n1);
+			retval /= n2;
+			return retval;
+		}
+		template <typename T>
+		static T binary_div(const mp_integer &n, const T &x, typename std::enable_if<std::is_floating_point<T>::value>::type * = nullptr)
+		{
+			return (static_cast<T>(n) / x);
+		}
+		template <typename T>
+		static T binary_div(const T &x, const mp_integer &n, typename std::enable_if<std::is_floating_point<T>::value>::type * = nullptr)
+		{
+			return (x / static_cast<T>(n));
+		}
 	public:
 		/// Defaulted default constructor.
 		/**
@@ -1755,7 +1823,7 @@ class mp_integer
 		 * - both \p T and \p U are piranha::mp_integer.
 		 * 
 		 * If one of the arguments is a floating-point value \p f of type \p F, the other argument will be converted to an instance of type \p F
-		 * and subtracted from \p f to generate the return value, which will then be of type \p F.
+		 * and subtracted from (or to) \p f to generate the return value, which will then be of type \p F.
 		 * 
 		 * @param[in] x first argument
 		 * @param[in] y second argument.
@@ -1951,6 +2019,78 @@ class mp_integer
 					::mpz_addmul(&m_int.dy,&n1.m_int.dy,&n2.m_int.dy);
 			}
 			return *this;
+		}
+		/// In-place division.
+		/**
+		 * \note
+		 * This operator is enabled only if \p T is an \ref interop "interoperable type" or piranha::mp_integer.
+		 * 
+		 * Divide by \p x in-place. If \p T is piranha::mp_integer or an integral type, the result will be truncated
+		 * (i.e., rounded towards 0). If \p T is a floating-point type, the following
+		 * sequence of operations takes place:
+		 * 
+		 * - \p this is converted to an instance \p f of type \p T via the conversion operator,
+		 * - \p f is divided by \p x,
+		 * - the result is assigned back to \p this.
+		 * 
+		 * @param[in] x argument for the division.
+		 * 
+		 * @return reference to \p this.
+		 * 
+		 * @throws unspecified any exception resulting from interoperating with floating-point types.
+		 * @throws piranha::zero_division_error if \p T is an integral type and \p x is zero.
+		 */
+		template <typename T>
+		typename std::enable_if<is_interoperable_type<T>::value || std::is_same<mp_integer,T>::value,
+			mp_integer &>::type operator/=(const T &x)
+		{
+			return in_place_div(x);
+		}
+		/// Generic in-place division with piranha::mp_integer.
+		/**
+		 * \note
+		 * This operator is enabled only if \p T is a non-const \ref interop "interoperable type".
+		 * 
+		 * Divide by a piranha::mp_integer in-place. This method will first compute <tt>x / n</tt>, cast it back to \p T via \p static_cast and finally assign the result to \p x.
+		 * 
+		 * @param[in,out] x first argument.
+		 * @param[in] n second argument.
+		 * 
+		 * @return reference to \p x.
+		 * 
+		 * @throws unspecified any exception thrown by the binary operator or by casting piranha::mp_integer to \p T.
+		 */
+		template <typename T>
+		friend typename std::enable_if<is_interoperable_type<T>::value && !std::is_const<T>::value,T &>::type
+			operator/=(T &x, const mp_integer &n)
+		{
+			x = static_cast<T>(x / n);
+			return x;
+		}
+		/// Generic binary division involving piranha::mp_integer.
+		/**
+		 * \note
+		 * This template operator is enabled only if either:
+		 * - \p T is piranha::mp_integer and \p U is an \ref interop "interoperable type",
+		 * - \p U is piranha::mp_integer and \p T is an \ref interop "interoperable type",
+		 * - both \p T and \p U are piranha::mp_integer.
+		 * 
+		 * If one of the arguments is a floating-point value \p f of type \p F, the other argument will be converted to an instance of type \p F
+		 * and divided by (or used as a dividend for) \p f to generate the return value, which will then be of type \p F.
+		 * 
+		 * @param[in] x first argument
+		 * @param[in] y second argument.
+		 * 
+		 * @return <tt>x / y</tt>.
+		 * 
+		 * @throws unspecified any exception resulting from interoperating with floating-point types.
+		 * @throws piranha::zero_division_error if both operands are of integral type and a division by zero occurs.
+		 */
+		template <typename T, typename U>
+		friend typename std::enable_if<are_binary_op_types<T,U>::value,typename deduce_binary_op_result_type<T,U>::type>::type
+			operator/(const T &x, const U &y)
+		{
+			return binary_div(x,y);
 		}
 	private:
 		detail::integer_union<NBits> m_int;
