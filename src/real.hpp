99701ee247fdbb69570f53aced84b81d368b79b3
Francesco Biscani <bluescarni@gmail.com>
2014-09-22 11:49:02 +0200

    real: move the swap function from std to the class namespace, as inline friend.
    This is the idiomatic modern way of providing a user-defined swap.

--- src/real.hpp
+++ src/real.hpp
@@ -879,6 +879,15 @@ class real: public detail::real_base<>
 			}
 			::mpfr_swap(m_value,other.m_value);
 		}
+		/// Swap function for piranha::real.
+		/**
+		 * @param[in] r1 first argument.
+		 * @param[in] r2 second argument.
+		 */
+		friend void swap(real &r1, real &r2) noexcept
+		{
+			r1.swap(r2);
+		}
 		/// Sign.
 		/**
 		 * @return 1 if <tt>this > 0</tt>, 0 if <tt>this == 0</tt> and -1 if <tt>this < 0</tt>. If \p this is NaN, zero will be returned.
@@ -2088,24 +2097,6 @@ inline real operator "" _r(const char *s)
 	return real(s);
 }
 
-}
-
-}
-
-namespace std
-{
-
-/// Specialization of \p std::swap for piranha::real.
-/**
- * @param[in] r1 first argument.
- * @param[in] r2 second argument.
- * 
- * @see piranha::real::swap()
- */
-template <>
-inline void swap(piranha::real &r1, piranha::real &r2) noexcept
-{
-	r1.swap(r2);
 }
 
 }
