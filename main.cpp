051beff955db6e3fd3f53756ca29fbe51fd80f51
Francesco Biscani <bluescarni@gmail.com>
2015-01-24 21:54:53 +0100

    main.cpp: fix sample main for recent changes.

--- main.cpp
+++ main.cpp
@@ -5,14 +5,14 @@ using namespace piranha;
 int main()
 {
 	environment env;
-	polynomial<double,int> p{"x"}, q{1}, mq{-1}, l{"y"};
+	polynomial<double,monomial<int>> p{"x"}, q{1}, mq{-1}, l{"y"};
 	std::cout << p << '\n';
 	std::cout << q << '\n';
 	std::cout << mq << '\n';
 	std::cout << (p + q) * (p + q) * (p + q) * (p + q) * (p + q) * (p + q) << '\n';
-	polynomial<polynomial<integer,int>,int> pp{"y"};
+	polynomial<polynomial<integer,monomial<int>>,monomial<int>> pp{"y"};
 	std::cout << (p + pp + 1) << '\n';
 	std::cout << ((-p + 1) * pp - pp) << '\n';
 	std::cout << (p * pp) << '\n';
-	std::cout << (-polynomial<double,int>{"x"} + 1) << '\n';
+	std::cout << (-polynomial<double,monomial<int>>{"x"} + 1) << '\n';
 }
