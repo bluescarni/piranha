b45226048e73975da2f2795ef5f40c211ad2e9fb
Francesco Biscani <bluescarni@gmail.com>
2015-01-25 00:01:15 +0100

    forwarding: forwarding constructor/assignment will be disabled if the argument derives from the class.
    This is equivalent to the change to the generic constructor in series a couple of commits ago.

--- src/forwarding.hpp
+++ src/forwarding.hpp
@@ -36,13 +36,13 @@
  * a variadic set of arguments whose size is greater than zero.
  * The constructor will perfectly forward the arguments to the base class
  * \p Base, and it is enabled only if the base class is constructible from the set of arguments.
- * In order to avoid clashes with copy/move constructors, in case the only argument is of type \p Derived
+ * In order to avoid clashes with copy/move constructors, in case the only argument derives from \p Derived
  * the constructor will be disabled.
  */
 #define PIRANHA_FORWARDING_CTOR(Derived,Base) \
 template <typename T_, typename ... Args_,typename = typename std::enable_if< \
 	std::is_constructible<Base,T_ &&,Args_ && ...>::value && \
-	(sizeof...(Args_) || !std::is_same<Derived,typename std::decay<T_>::type>::value)>::type> \
+	(sizeof...(Args_) || !std::is_base_of<Derived,typename std::decay<T_>::type>::value)>::type> \
 	explicit Derived(T_ &&arg0, Args_ && ... args):Base(std::forward<T_>(arg0),std::forward<Args_>(args)...) {}
 
 /// Assignment-forwarding macro.
@@ -50,13 +50,13 @@ template <typename T_, typename ... Args_,typename = typename std::enable_if< \
  * This macro will declare and define a generic assignment operator for class \p Derived.
  * The operator will perfectly forward the argument to the base class
  * \p Base, and it is enabled only if the base class is assignable from the generic argument.
- * In order to avoid clashes with copy/move assignment operators, in case the argument is of type \p Derived
+ * In order to avoid clashes with copy/move assignment operators, in case the argument derives from \p Derived
  * the operator will be disabled.
  */
 #define PIRANHA_FORWARDING_ASSIGNMENT(Derived,Base) \
 template <typename T_> \
 typename std::enable_if<std::is_assignable<Base &,T_ &&>::value && \
-	!std::is_same<Derived,typename std::decay<T_>::type>::value, \
+	!std::is_base_of<Derived,typename std::decay<T_>::type>::value, \
 	Derived &>::type operator=(T_ &&arg) \
 { \
 	Base::operator=(std::forward<T_>(arg)); \
