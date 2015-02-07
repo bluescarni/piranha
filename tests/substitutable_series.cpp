7cff33961ca7b2a5bbe4c753a342a41be80db2b6
Francesco Biscani <bluescarni@gmail.com>
2015-02-07 16:04:54 +0100

    subs_series: more testing, now with subs only on cf.

--- tests/substitutable_series.cpp
+++ tests/substitutable_series.cpp
@@ -23,10 +23,20 @@
 #define BOOST_TEST_MODULE substitutable_series_test
 #include <boost/test/unit_test.hpp>
 
+#include <algorithm>
+#include <array>
+#include <cstddef>
+#include <functional>
+#include <iterator>
+#include <stdexcept>
 #include <string>
 
+#include "../src/config.hpp"
 #include "../src/environment.hpp"
+#include "../src/exceptions.hpp"
 #include "../src/forwarding.hpp"
+#include "../src/is_key.hpp"
+#include "../src/key_is_multipliable.hpp"
 #include "../src/math.hpp"
 #include "../src/monomial.hpp"
 #include "../src/mp_integer.hpp"
@@ -34,17 +44,19 @@
 #include "../src/real.hpp"
 #include "../src/serialization.hpp"
 #include "../src/series.hpp"
+#include "../src/symbol_set.hpp"
+#include "../src/term.hpp"
 
 using namespace piranha;
 
-template <typename Cf, typename Expo>
-class g_series_type: public substitutable_series<series<Cf,monomial<Expo>,g_series_type<Cf,Expo>>,g_series_type<Cf,Expo>>
+template <typename Cf, typename Key>
+class g_series_type: public substitutable_series<series<Cf,Key,g_series_type<Cf,Key>>,g_series_type<Cf,Key>>
 {
-		using base = substitutable_series<series<Cf,monomial<Expo>,g_series_type<Cf,Expo>>,g_series_type<Cf,Expo>>;
+		using base = substitutable_series<series<Cf,Key,g_series_type<Cf,Key>>,g_series_type<Cf,Key>>;
 		PIRANHA_SERIALIZE_THROUGH_BASE(base)
 	public:
 		template <typename Cf2>
-		using rebind = g_series_type<Cf2,Expo>;
+		using rebind = g_series_type<Cf2,Key>;
 		g_series_type() = default;
 		g_series_type(const g_series_type &) = default;
 		g_series_type(g_series_type &&) = default;
@@ -54,7 +66,7 @@ class g_series_type: public substitutable_series<series<Cf,monomial<Expo>,g_seri
 			// Insert the symbol.
 			this->m_symbol_set.add(name);
 			// Construct and insert the term.
-			this->insert(term_type(Cf(1),typename term_type::key_type{Expo(1)}));
+			this->insert(term_type(Cf(1),typename term_type::key_type{typename Key::value_type(1)}));
 		}
 		g_series_type &operator=(const g_series_type &) = default;
 		g_series_type &operator=(g_series_type &&) = default;
@@ -62,17 +74,66 @@ class g_series_type: public substitutable_series<series<Cf,monomial<Expo>,g_seri
 		PIRANHA_FORWARDING_ASSIGNMENT(g_series_type,base)
 };
 
+// An alternative monomial class with no suitable subs() method.
+template <class T>
+class new_monomial: public monomial<T>
+{
+		using base = monomial<T>;
+	public:
+		using base::base;
+		new_monomial merge_args(const symbol_set &orig_args, const symbol_set &new_args) const
+		{
+			auto tmp = static_cast<const base *>(this)->merge_args(orig_args,new_args);
+			new_monomial retval;
+			std::copy(tmp.begin(),tmp.end(),std::back_inserter(retval));
+			return retval;
+		}
+		new_monomial trim(const symbol_set &trim_args, const symbol_set &orig_args) const
+		{
+			auto tmp = static_cast<const base *>(this)->trim(trim_args,orig_args);
+			new_monomial retval;
+			std::copy(tmp.begin(),tmp.end(),std::back_inserter(retval));
+			return retval;
+		}
+		template <typename Cf>
+		static void multiply(std::array<term<Cf,new_monomial>,base::multiply_arity> &res, const term<Cf,new_monomial> &t1,
+			const term<Cf,new_monomial> &t2, const symbol_set &args)
+		{
+			term<Cf,new_monomial> &t = res[0u];
+			if(unlikely(t1.m_key.size() != args.size())) {
+				piranha_throw(std::invalid_argument,"invalid size of arguments set");
+			}
+			t.m_cf = t1.m_cf * t2.m_cf;
+			t1.m_key.vector_add(t.m_key,t2.m_key);
+		}
+};
+
+namespace std
+{
+
+template <typename T>
+struct hash<new_monomial<T>>
+{
+	std::size_t operator()(const new_monomial<T> &m) const
+	{
+		return m.hash();
+	}
+};
+
+}
+
 BOOST_AUTO_TEST_CASE(subs_series_subs_test)
 {
 	environment env;
 	// Substitution on key only.
-	using stype0 = g_series_type<rational,int>;
+	using stype0 = g_series_type<rational,monomial<int>>;
 	// Type trait checks.
 	BOOST_CHECK((has_subs<stype0,int>::value));
 	BOOST_CHECK((has_subs<stype0,double>::value));
 	BOOST_CHECK((has_subs<stype0,integer>::value));
 	BOOST_CHECK((has_subs<stype0,rational>::value));
 	BOOST_CHECK((has_subs<stype0,real>::value));
+	{
 	BOOST_CHECK((!has_subs<stype0,std::string>::value));
 	stype0 x{"x"}, y{"y"}, z{"z"};
 	auto tmp = (x + y).subs("x",2);
@@ -83,17 +144,53 @@ BOOST_AUTO_TEST_CASE(subs_series_subs_test)
 	auto tmp2 = (x + y).subs("x",2.);
 	BOOST_CHECK_EQUAL(tmp2,y + 2.);
 	BOOST_CHECK(tmp2.is_identical(math::subs(x+y,"x",2.)));
-	BOOST_CHECK((std::is_same<decltype(tmp2),g_series_type<double,int>>::value));
+	BOOST_CHECK((std::is_same<decltype(tmp2),g_series_type<double,monomial<int>>>::value));
 	auto tmp3 = (3*x + y*y/7).subs("y",2/5_q);
 	BOOST_CHECK(tmp3.is_identical(math::subs(3*x + y*y/7,"y",2/5_q)));
 	BOOST_CHECK((std::is_same<decltype(tmp3),stype0>::value));
 	BOOST_CHECK_EQUAL(tmp3,3*x + 2/5_q * 2/5_q / 7);
 	auto tmp4 = (3*x + y*y/7).subs("y",2.123_r);
 	BOOST_CHECK(tmp4.is_identical(math::subs(3*x + y*y/7,"y",2.123_r)));
-	BOOST_CHECK((std::is_same<decltype(tmp4),g_series_type<real,int>>::value));
+	BOOST_CHECK((std::is_same<decltype(tmp4),g_series_type<real,monomial<int>>>::value));
 	BOOST_CHECK_EQUAL(tmp4,3*x + math::pow(2.123_r,2) / 7);
 	auto tmp5 = (3*x + y*y/7).subs("y",-2_z);
 	BOOST_CHECK(tmp5.is_identical(math::subs(3*x + y*y/7,"y",-2_z)));
 	BOOST_CHECK((std::is_same<decltype(tmp5),stype0>::value));
 	BOOST_CHECK_EQUAL(tmp5,3*x + math::pow(-2_z,2) / 7_q);
+	}
+	// Subs on cf only.
+	using stype1 = g_series_type<stype0,new_monomial<int>>;
+	BOOST_CHECK((is_key<stype1::term_type::key_type>::value));
+	BOOST_CHECK((key_is_multipliable<rational,stype1::term_type::key_type>::value));
+	BOOST_CHECK((!key_has_subs<stype1::term_type::key_type,rational>::value));
+	BOOST_CHECK((has_subs<stype1,int>::value));
+	BOOST_CHECK((has_subs<stype1,double>::value));
+	BOOST_CHECK((has_subs<stype1,integer>::value));
+	BOOST_CHECK((has_subs<stype1,rational>::value));
+	BOOST_CHECK((has_subs<stype1,real>::value));
+	BOOST_CHECK((!has_subs<stype1,std::string>::value));
+	{
+	stype1 x{stype0{"x"}}, y{stype0{"y"}}, z{stype0{"z"}};
+	auto tmp = (x + y).subs("x",2);
+	BOOST_CHECK_EQUAL(tmp,y + 2);
+	BOOST_CHECK(tmp.is_identical(math::subs(x+y,"x",2)));
+	BOOST_CHECK(tmp.is_identical(y + 2 + x - x));
+	BOOST_CHECK((std::is_same<decltype(tmp),stype1>::value));
+	auto tmp2 = (x + y).subs("x",2.);
+	BOOST_CHECK_EQUAL(tmp2,y + 2.);
+	BOOST_CHECK(tmp2.is_identical(math::subs(x+y,"x",2.)));
+	BOOST_CHECK((std::is_same<decltype(tmp2),g_series_type<g_series_type<double,monomial<int>>,new_monomial<int>>>::value));
+	auto tmp3 = (3*x + y*y/7).subs("y",2/5_q);
+	BOOST_CHECK(tmp3.is_identical(math::subs(3*x + y*y/7,"y",2/5_q)));
+	BOOST_CHECK((std::is_same<decltype(tmp3),stype1>::value));
+	BOOST_CHECK_EQUAL(tmp3,3*x + 2/5_q * 2/5_q / 7);
+	auto tmp4 = (3*x + y*y/7).subs("y",2.123_r);
+	BOOST_CHECK(tmp4.is_identical(math::subs(3*x + y*y/7,"y",2.123_r)));
+	BOOST_CHECK((std::is_same<decltype(tmp4),g_series_type<g_series_type<real,monomial<int>>,new_monomial<int>>>::value));
+	BOOST_CHECK_EQUAL(tmp4,3*x + math::pow(2.123_r,2) / 7);
+	auto tmp5 = (3*x + y*y/7).subs("y",-2_z);
+	BOOST_CHECK(tmp5.is_identical(math::subs(3*x + y*y/7,"y",-2_z)));
+	BOOST_CHECK((std::is_same<decltype(tmp5),stype1>::value));
+	BOOST_CHECK_EQUAL(tmp5,3*x + math::pow(-2_z,2) / 7_q);
+	}
 }
