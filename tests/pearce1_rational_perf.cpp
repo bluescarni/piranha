ed12a0ec81798aaea02af641dee7f2e3f87bfc13
Francesco Biscani <bluescarni@gmail.com>
2015-08-13 16:24:02 +0200

    Add rational versions of fateman1 and pearce1 benchs.

---
+++ tests/pearce1_rational_perf.cpp
@@ -0,0 +1,48 @@
+/***************************************************************************
+ *   Copyright (C) 2009-2011 by Francesco Biscani                          *
+ *   bluescarni@gmail.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "pearce1.hpp"
+
+#define BOOST_TEST_MODULE pearce1_test
+#include <boost/test/unit_test.hpp>
+
+#include <boost/lexical_cast.hpp>
+
+#include "../src/environment.hpp"
+#include "../src/kronecker_monomial.hpp"
+#include "../src/mp_rational.hpp"
+#include "../src/settings.hpp"
+
+using namespace piranha;
+
+// Pearce's polynomial multiplication test number 1. Calculate:
+// f * g
+// where
+// f = (1 + x + y + 2*z**2 + 3*t**3 + 5*u**5)**12
+// g = (1 + u + t + 2*z**2 + 3*y**3 + 5*x**5)**12
+
+BOOST_AUTO_TEST_CASE(pearce1_test)
+{
+	environment env;
+	if (boost::unit_test::framework::master_test_suite().argc > 1) {
+		settings::set_n_threads(boost::lexical_cast<unsigned>(boost::unit_test::framework::master_test_suite().argv[1u]));
+	}
+	BOOST_CHECK_EQUAL((pearce1<rational,kronecker_monomial<>>().size()),5821335u);
+}
