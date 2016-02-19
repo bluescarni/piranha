69a3a0097524d40e4b2e8c400ac75b933e67a7b9
Francesco Biscani <bluescarni@gmail.com>
2016-02-19 23:01:36 +0100

    Try to start using a bash script for travis.
    [skip appveyor]

---
+++ tools/install_travis.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Exit on error
+set -e
+# Echo each command
+set -x
+
+if [[ "${BUILD_TYPE}" == "Debug" ]]; then
+    if [[ "${PIRANHA_COMPILER}" == "gcc" ]]; then
+        cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS=--coverage -DPIRANHA_TEST_SPLIT=yes -DPIRANHA_TEST_SPLIT_NUM=${SPLIT_TEST_NUM} ../;
+    elif [[ "${PIRANHA_COMPILER}" == "clang" ]]; then
+        cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_CXX_FLAGS="-fsanitize=address" -DPIRANHA_TEST_SPLIT=yes -DPIRANHA_TEST_SPLIT_NUM=${SPLIT_TEST_NUM} ../;
+    fi
+    make;
+    ctest -E "thread" -V;
+elif [[ "${BUILD_TYPE}" == "Release" ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Release ../;
+    make;
+    ctest -E "gastineau|perminov" -V;
+elif [[ "${BUILD_TYPE}" == "Python2" ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_PYRANHA=yes -DBUILD_TESTS=no -DCMAKE_CXX_FLAGS_DEBUG=-g0 -DCMAKE_CXX_FLAGS=-Os -DCMAKE_INSTALL_PREFIX=/home/travis/.local ../;
+    make install;
+    pip install --user mpmath;
+    python -c "import pyranha.test; pyranha.test.run_test_suite()";
+elif [[ "${BUILD_TYPE}" == "Python3" ]]; then
+    cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_PYRANHA=yes -DBUILD_TESTS=no -DCMAKE_CXX_FLAGS_DEBUG=-g0 -DCMAKE_CXX_FLAGS=-Os -DCMAKE_INSTALL_PREFIX=/home/travis/.local -DBoost_PYTHON_LIBRARY_RELEASE=/usr/lib/x86_64-linux-gnu/libboost_python-py32.so -DBoost_PYTHON_LIBRARY_DEBUG=/usr/lib/x86_64-linux-gnu/libboost_python-py32.so -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.2 -DPYTHON_LIBRARY=/usr/lib/libpython3.2mu.so ../;
+    make install;
+    wget "http://mpmath.org/files/mpmath-0.19.tar.gz";
+    tar xzf mpmath-0.19.tar.gz;
+    cd mpmath-0.19;
+    python3 setup.py install --user;
+    cd ..;
+    python3 -c "import pyranha.test; pyranha.test.run_test_suite()";
+fi
