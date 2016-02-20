47f6da3894b3ce34722d3f782ae7d6b74ca2cfd1
Francesco Biscani <bluescarni@gmail.com>
2016-02-20 15:48:05 +0100

    Initial scaffolding for sphinx doc and alternative approach to suppress push output.
    [skip appveyor]

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -22,11 +22,41 @@ elif [[ "${BUILD_TYPE}" == "Release" ]]; then
 elif [[ "${BUILD_TYPE}" == "Python2" ]]; then
     cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_PYRANHA=yes -DBUILD_TESTS=no -DCMAKE_CXX_FLAGS_DEBUG=-g0 -DCMAKE_CXX_FLAGS=-Os -DCMAKE_INSTALL_PREFIX=/home/travis/.local ../;
     make install;
+    # Install mpmath via pip.
     pip install --user mpmath;
     python -c "import pyranha.test; pyranha.test.run_test_suite()";
+    # Install sphinx.
+    pip install --user sphinx
+    export PATH=$PATH:/home/travis/.local/bin
+    cd ../doc/sphinx;
+    export SPHINX_OUTPUT=`make html 2>&1 >/dev/null`;
+    if [[ "${SPHINX_OUTPUT}" != "" ]]; then
+        echo "Sphinx encountered some problem:";
+        echo "${SPHINX_OUTPUT}";
+        exit 1;
+    fi
+    echo "Sphinx ran successfully";
+    # Move out the resulting documentation.
+    mv _build/html /home/travis/sphinx;
+    # Checkout a new copy of the repo, for pushing to gh-pages.
+    cd ../../../;
+    git config --global push.default simple
+    git config --global user.name "Travis CI"
+    git config --global user.email "bluescarni@gmail.com"
+    set +x
+    git clone "https://${GH_TOKEN}@github.com/bluescarni/piranha.git" piranha_gh_pages 2>&1 |cat - | > /dev/null
+    set -x
+    cd piranha_gh_pages
+    git checkout -b gh-pages --track origin/gh-pages;
+    git rm -fr sphinx;
+    mv /home/travis/sphinx .;
+    git add sphinx;
+    git commit -m "Update Sphinx documentation [skip ci]."
+    git push 2>&1 |cat - | > /dev/null
 elif [[ "${BUILD_TYPE}" == "Python3" ]]; then
     cmake -DCMAKE_BUILD_TYPE=Debug -DBUILD_PYRANHA=yes -DBUILD_TESTS=no -DCMAKE_CXX_FLAGS_DEBUG=-g0 -DCMAKE_CXX_FLAGS=-Os -DCMAKE_INSTALL_PREFIX=/home/travis/.local -DBoost_PYTHON_LIBRARY_RELEASE=/usr/lib/x86_64-linux-gnu/libboost_python-py32.so -DBoost_PYTHON_LIBRARY_DEBUG=/usr/lib/x86_64-linux-gnu/libboost_python-py32.so -DPYTHON_EXECUTABLE=/usr/bin/python3 -DPYTHON_INCLUDE_DIR=/usr/include/python3.2 -DPYTHON_LIBRARY=/usr/lib/libpython3.2mu.so ../;
     make install;
+    # Install mpmath manually.
     wget "http://mpmath.org/files/mpmath-0.19.tar.gz";
     tar xzf mpmath-0.19.tar.gz;
     cd mpmath-0.19;
@@ -40,7 +70,7 @@ elif [[ "${BUILD_TYPE}" == "Tutorial" ]]; then
 elif [[ "${BUILD_TYPE}" == "Doxygen" ]]; then
     # Configure.
     cmake ../;
-    # Install recent version of Doxygen locally.
+    # Install a recent version of Doxygen locally.
     wget "http://ftp.stack.nl/pub/users/dimitri/doxygen-1.8.10.src.tar.gz";
     tar xzf doxygen-1.8.10.src.tar.gz;
     cd doxygen-1.8.10;
@@ -74,5 +104,5 @@ elif [[ "${BUILD_TYPE}" == "Doxygen" ]]; then
     mv /home/travis/doxygen .;
     git add doxygen;
     git commit -m "Update Doxygen documentation [skip ci]."
-    git push 2>&1 |cat - | > /dev/null
+    git push -q
 fi
