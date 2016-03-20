421647da5831539d680c4e52becfa3517a6c194d
Francesco Biscani <bluescarni@gmail.com>
2016-03-20 10:14:40 +0100

    rational_function: initial implementation work, doc and testing.
    [skip ci]

--- src/rational_function.hpp
+++ src/rational_function.hpp
@@ -35,6 +35,7 @@ see https://www.gnu.org/licenses/. */
 #include <utility>
 
 #include "config.hpp"
+#include "exceptions.hpp"
 #include "math.hpp"
 #include "mp_integer.hpp"
 #include "mp_rational.hpp"
@@ -45,13 +46,29 @@ namespace piranha
 {
 
 /// Rational function.
+/**
+ * This class represents rational functions, that is, mathematical object of the form
+ * \f[
+ * \frac{f\left(x_0,x_1,\ldots\right)}{g\left(x_0,x_1,\ldots\right)},
+ * \f]
+ * where \f$f\left(x_0,x_1,\ldots\right)\f$ and \f$g\left(x_0,x_1,\ldots\right)\f$ are polynomials in the variables \f$x_0,x_1,\ldots\f$ over \f$\mathbb{Z}\f$.
+ * The monomial representation is determined by the \p Key template parameter. Only monomial types with integral exponents are allowed; signed exponent
+ * types are allowed, but if a negative exponent is ever generated or encountered while operating on a rational function an error will be produced.
+ *
+ * Internally, a piranha::rational_function consists of a numerator and a denominator represented via piranha::polynomial with piranha::integer coefficients.
+ * Rational functions are always kept in a canonical form defined by the following properties:
+ * - numerator and denominator are coprime,
+ * - zero is always represented as <tt>0 / 1</tt>,
+ * - the leading term of the denominator always has a positive coefficient.
+ */
+// TODO: document move semantics.
 template <typename Key>
 class rational_function
 {
 		// Shortcut for supported integral type.
 		template <typename T>
 		using is_integral = std::integral_constant<bool,
-			detail::is_mp_integer<T>::value || std::is_integral<T>::value>;
+			std::is_same<integer,T>::value || std::is_integral<T>::value>;
 		PIRANHA_TT_CHECK(detail::is_polynomial_key,Key);
 		static_assert(is_integral<typename Key::value_type>::value,"The exponent type must be an integral type.");
 		// Shortcut from C++14.
@@ -63,32 +80,51 @@ class rational_function
 		/// The counterpart of rational_function::p_type with rational coefficients.
 		using q_type = polynomial<rational,Key>;
 	private:
-		// Generic ctors utils.
-		// Ctor from p_type, integrals and string.
-		template <typename T, typename std::enable_if<std::is_same<decay_t<T>,p_type>::value || is_integral<decay_t<T>>::value ||
-			std::is_same<decay_t<T>,std::string>::value ||
-			std::is_same<decay_t<T>,char *>::value ||
-			std::is_same<decay_t<T>,const char *>::value,int>::type = 0>
-		void dispatch_unary_ctor(T &&x)
+		// Canonicalisation.
+		static std::pair<p_type,p_type> canonicalise_impl(const p_type &n, const p_type &d)
 		{
-			m_num = std::forward<T>(x);
-			m_den = p_type{1};
-			// Check for negative expos in the num if cting from a polynomial.
-			if (std::is_same<decay_t<T>,p_type>::value) {
-				detail::poly_expo_checker(m_num);
+			// First let's check for negative exponents.
+			detail::poly_expo_checker(n);
+			detail::poly_expo_checker(d);
+			// Handle a zero divisor.
+			if (unlikely(math::is_zero(d))) {
+				piranha_throw(zero_division_error,"null denominator in rational function");
 			}
+			// If the numerator is null, return {0,1}.
+			if (math::is_zero(n)) {
+				return std::make_pair(p_type{},p_type{1});
+			}
+			// NOTE: maybe these checks should go directly into the poly GCD routine. Keep it
+			// in mind for the future.
+			// If the denominator is unitary, just return the input values.
+			if (math::is_unitary(d)) {
+				return std::make_pair(n,d);
+			}
+			// Handle single-coefficient polys.
+			if (n.is_single_coefficient() && d.is_single_coefficient()) {
+				piranha_assert(n.size() == 1u && d.size() == 1u);
+				rational tmp{n._container().begin()->m_cf,d._container().begin()->m_cf};
+				return std::make_pair(p_type{tmp.num()},p_type{tmp.den()});
+			}
+			// Compute the GCD and create the return values.
+			auto g = math::gcd(n,d);
+			auto retval = std::make_pair(n / g, d / g);
+			// Check if we need to adjust the sign of the leading term
+			// of the denominator.
+			if (detail::poly_lterm(retval.second)->m_cf.sign() < 0) {
+				math::negate(retval.first);
+				math::negate(retval.second);
+			}
+			return retval;
 		}
-		// Ctor from rationals.
-		template <typename T, typename std::enable_if<detail::is_mp_rational<decay_t<T>>::value,int>::type = 0>
-		void dispatch_unary_ctor(const T &q)
+		// Utility function to convert a q_type to a pair (p_type,integer), representing the numerator
+		// and denominator of a rational function.
+		static std::pair<p_type,integer> q_to_p_type(const q_type &q)
 		{
-			m_num = q.num();
-			m_den = q.den();
-		}
-		// Ctor from q_type.
-		template <typename T, typename std::enable_if<std::is_same<decay_t<T>,q_type>::value,int>::type = 0>
-		void dispatch_unary_ctor(const T &q)
-		{
+			// Init the numerator.
+			p_type ret_p;
+			ret_p._container().rehash(q._container().bucket_count());
+			ret_p.set_symbol_set(q.get_symbol_set());
 			// Compute the least common multiplier.
 			integer lcm{1};
 			// The GCD.
@@ -101,17 +137,46 @@ class rational_function
 			// All these computations involve only positive numbers,
 			// the GCD must always be positive.
 			piranha_assert(lcm.sign() == 1);
-			// Construct the numerator.
-			// Rehash according to q.
-			m_num._container().rehash(q._container().bucket_count());
-			m_num.set_symbol_set(q.get_symbol_set());
+			// Fill in the numerator.
 			for (const auto &t: q._container()) {
 				using term_type = typename p_type::term_type;
 				// NOTE: possibility of unique insertion here.
-				m_num.insert(term_type{lcm / t.m_cf.den() * t.m_cf.num(),t.m_key});
+				// NOTE: possibility of exact division.
+				ret_p.insert(term_type{lcm / t.m_cf.den() * t.m_cf.num(),t.m_key});
 			}
-			// Construct the denominator.
-			m_den = std::move(lcm);
+			return std::make_pair(std::move(ret_p),std::move(lcm));
+		}
+		// Generic ctors utils.
+		// Enabler for the constructor from p_type, integral and string.
+		template <typename T>
+		using pzs_enabler = std::integral_constant<bool,std::is_same<decay_t<T>,p_type>::value || is_integral<decay_t<T>>::value ||
+			std::is_same<decay_t<T>,std::string>::value ||
+			std::is_same<decay_t<T>,char *>::value ||
+			std::is_same<decay_t<T>,const char *>::value>;
+		// Ctor from p_type, integrals and string.
+		template <typename T, typename std::enable_if<pzs_enabler<T>::value,int>::type = 0>
+		void dispatch_unary_ctor(T &&x)
+		{
+			m_num = std::forward<T>(x);
+			m_den = p_type{1};
+			// Check for negative expos in the num if cting from a polynomial.
+			if (std::is_same<decay_t<T>,p_type>::value) {
+				detail::poly_expo_checker(m_num);
+			}
+		}
+		// Ctor from rationals.
+		void dispatch_unary_ctor(const rational &q)
+		{
+			// NOTE: here we are assuming that q is canonicalised, as it should be.
+			m_num = q.num();
+			m_den = q.den();
+		}
+		// Ctor from q_type.
+		void dispatch_unary_ctor(const q_type &q)
+		{
+			auto res = q_to_p_type(q);
+			m_num = std::move(res.first);
+			m_den = std::move(res.second);
 			// Check the numerator's exponents.
 			detail::poly_expo_checker(m_num);
 		}
@@ -122,6 +187,53 @@ class rational_function
 			// as the dispatcher does not support that.
 			detail::true_tt<decltype(std::declval<U &>().dispatch_unary_ctor(std::declval<const decay_t<T> &>()))>::value
 		,int>::type;
+		// Binary constructors.
+		// Enabler for construction from pzs. We require T and U to be the same, after decay.
+		template <typename T, typename U>
+		using pzs_enabler2 = std::integral_constant<bool,pzs_enabler<T>::value && std::is_same<decay_t<T>,decay_t<U>>::value>;
+		template <typename T, typename U, typename std::enable_if<pzs_enabler2<T,U>::value,int>::type = 0>
+		void dispatch_binary_ctor(T &&x, U &&y)
+		{
+			m_num = std::forward<T>(x);
+			m_den = std::forward<U>(y);
+			// When constructing from polys or integrals, we need to canonicalise.
+			// NOTE: normally we should *not* set m_num/m_den and canonicalise afterwards, as this
+			// is not exception safe. Here it is a special case because this function is called from
+			// a ctor only, thus if something goes wrong in the canonicalisation m_num/m_den will be destroyed
+			// in the cleanup.
+			if (std::is_same<decay_t<T>,p_type>::value || is_integral<decay_t<T>>::value) {
+				canonicalise();
+			}
+		}
+		void dispatch_binary_ctor(const q_type &n, const q_type &d)
+		{
+			auto res_n = q_to_p_type(n);
+			auto res_d = q_to_p_type(d);
+			m_num = std::move(res_n.first);
+			m_den = std::move(res_d.first);
+			m_num *= std::move(res_d.second);
+			m_den *= std::move(res_n.second);
+			// Canonicalise.
+			canonicalise();
+		}
+		void dispatch_binary_ctor(const rational &n, const rational &d)
+		{
+			auto tmp = n / d;
+			m_num = tmp.num();
+			m_den = tmp.den();
+		}
+		// Enabler for generic binary ctor.
+		template <typename T, typename U, typename V>
+		using binary_ctor_enabler = typename std::enable_if<
+			// NOTE: this should disable the ctor if T derives from rational_function,
+			// as the dispatcher does not support that.
+			detail::true_tt<decltype(std::declval<V &>().dispatch_binary_ctor(std::declval<const decay_t<T> &>(),std::declval<const decay_t<U> &>()))>::value
+		,int>::type;
+		// Enabler for generic assignment.
+		// Here we are re-using the enabler for generic unary construction. This should make sure that
+		// the generic assignment operator is never used when T is rational_function.
+		template <typename T, typename U>
+		using generic_assignment_enabler = unary_ctor_enabler<T,U>;
 	public:
 		/// Default constructor.
 		/**
@@ -136,17 +248,70 @@ class rational_function
 		rational_function(rational_function &&) = default;
 		/// Generic unary constructor.
 		/**
-		 * 
+		 * \note
+		 * This constructor is enabled if the decay type of \p T is either:
+		 * - rational_function::p_type,
+		 * - rational_function::q_type,
+		 * - a C++ integral type, piranha::integer or piranha::rational,
+		 * - a string type.
+		 *
+		 * The constructor operates as follows:
+		 * - if \p T is rational_function::p_type, a C++ integral type, piranha::integer or a string type, then the numerator
+		 *   is constructed from \p x, and the denominator is set to 1;
+		 * - if \p T is piranha::rational, then the numerator is constructed from the numerator of \p x, and the denominator
+		 *   from the denominator of \p x;
+		 * - if \p T is rational_function::q_type, then, after construction, \p this will be mathematically equivalent to \p x (that is, the coefficients
+		 *   of \p x are multiplied by their LCM and the result is used to construct the numerator, and the denominator is constructed from the LCM
+		 *   of the coefficients).
+		 *
+		 * @param[in] x construction argument.
+		 *
 		 * @throws std::invalid_argument if, when constructing from rational_function::p_type or rational_function::q_type, a negative exponent is
 		 * encountered.
 		 * @throws unspecified any exception thrown by:
 		 * - the constructors and the assignment operators of rational_function::p_type,
+		 * - the constructors of the term type of rational_function::p_type,
 		 * - the public interface of piranha::series and piranha::hash_set.
 		 */
 		template <typename T, typename U = rational_function, unary_ctor_enabler<T,U> = 0>
 		explicit rational_function(T &&x)
 		{
 			dispatch_unary_ctor(std::forward<T>(x));
+		}
+		/// Generic binary constructor.
+		/**
+		 * \note
+		 * This constructor is enabled only if the decay types of \p T and \p U are the same type \p Td. \p Td must be either:
+		 * - rational_function::p_type,
+		 * - rational_function::q_type,
+		 * - a C++ integral type, piranha::integer or piranha::rational,
+		 * - a string type.
+		 *
+		 * The constructor operates as follows:
+		 * - if \p Td is rational_function::p_type, a C++ integral type, piranha::integer or a string type, then the numerator
+		 *   is constructed from \p x and the denominator from \p y;
+		 * - if \p Td is piranha::rational, then the first rational is divided by the second and the result's numerator and denominator
+		 *   are used to construct the numerator and denominator of \p this;
+		 * - if \p Td is rational_function::q_type, then, after construction, \p this will be mathematically equivalent
+		 *   to <tt>x / y</tt> (that is, \p x and \p y are both multiplied by <tt>lx * ly</tt>, the product of the LCMs of their coefficients;
+		 *   the resulting polynomials are used to initialise the numerator and denominator of \p this).
+		 *
+		 * @param[in] x first argument.
+		 * @param[in] y second argument.
+		 *
+		 * @throws std::invalid_argument if, when constructing from rational_function::p_type or rational_function::q_type, a negative exponent is
+		 * encountered.
+		 * @throws unspecified any exception thrown by:
+		 * - the constructors, the assignment and the multiplication operators of rational_function::p_type,
+		 * - the division operator of piranha::rational,
+		 * - the constructors of the term type of rational_function::p_type,
+		 * - the public interface of piranha::series and piranha::hash_set,
+		 * - piranha::rational_function::canonicalise().
+		 */
+		template <typename T, typename U, typename V = rational_function, binary_ctor_enabler<T,U,V> = 0>
+		explicit rational_function(T &&x, U &&y)
+		{
+			dispatch_binary_ctor(std::forward<T>(x),std::forward<U>(y));
 		}
 		/// Copy-assignment operator.
 		/**
@@ -159,14 +324,70 @@ class rational_function
 		rational_function &operator=(const rational_function &other)
 		{
 			if (likely(&other != this)) {
-				*this = rational(other);
+				*this = rational_function(other);
 			}
 			return *this;
 		}
 		/// Defaulted move-assignment operator.
 		rational_function &operator=(rational_function &&) = default;
+		/// Generic assignment operator.
+		/**
+		 * \note
+		 * This operator is enabled only if the corresponding generic unary constructor is enabled.
+		 *
+		 * The operator is implemented as the assignment from a piranha::rational_function constructed
+		 * from \p x.
+		 *
+		 * @param[in] x assignment argument.
+		 *
+		 * @return a reference to \p this.
+		 *
+		 * @throws unspecified any exception thrown by the invoked constructor.
+		 */
+		template <typename T, typename U = rational_function, generic_assignment_enabler<T,U> = 0>
+		rational_function &operator=(T &&x)
+		{
+			// Check this is never invoked when T is a rational function.
+			static_assert(!std::is_base_of<rational_function,decay_t<T>>::value,"Type error.");
+			return *this = rational_function(std::forward<T>(x));
+		}
 		/// Defaulted destructor.
 		~rational_function() = default;
+		/// Canonicalisation.
+		/**
+		 * This method will put \p this in canonical form. Normally, it is never necessary to call this method,
+		 * unless low-level methods that do not keep \p this in canonical form have been invoked.
+		 *
+		 * If any exception is thrown by this method, \p this will not be modified.
+		 *
+		 * @throws piranha::zero_division_error if the numerator is zero.
+		 * @throws std::invalid_argument if either the numerator or the denominator have a negative exponent.
+		 * @throws unspecified any exception thrown by construction, division and GCD operations involving objects of
+		 * type rational_function::p_type.
+		 */
+		void canonicalise()
+		{
+			auto res = canonicalise_impl(m_num,m_den);
+			// This is noexcept.
+			m_num = std::move(res.first);
+			m_den = std::move(res.second);
+		}
+		/// Numerator.
+		/**
+		 * @return a const reference to the numerator.
+		 */
+		const p_type &num() const
+		{
+			return m_num;
+		}
+		/// Denominator.
+		/**
+		 * @return a const reference to the denominator.
+		 */
+		const p_type &den() const
+		{
+			return m_den;
+		}
 		/// Stream operator.
 		/**
 		 * Will stream to \p os a human-readable representation of \p r.
--- tests/rational_function.cpp
+++ tests/rational_function.cpp
@@ -55,9 +55,43 @@ struct constructor_tester
 		using r_type = rational_function<Key>;
 		using p_type = typename r_type::p_type;
 		using q_type = typename r_type::q_type;
+		p_type x{"x"}, y{"y"}, z{"z"};
+		q_type xq{"x"}, yq{"y"}, zq{"z"};
+		// Standard constructors.
+		r_type r;
+		BOOST_CHECK_EQUAL(r.num(),0);
+		BOOST_CHECK_EQUAL(r.den(),1);
+		BOOST_CHECK(r.num().get_symbol_set().size() == 0u);
+		BOOST_CHECK(r.den().get_symbol_set().size() == 0u);
+		r = r_type{5};
+		auto s = r;
+		BOOST_CHECK_EQUAL(s.num(),5);
+		BOOST_CHECK_EQUAL(s.den(),1);
+		BOOST_CHECK(s.num().get_symbol_set().size() == 0u);
+		BOOST_CHECK(s.den().get_symbol_set().size() == 0u);
+		// Check status of moved-from objects.
+		r_type t = std::move(s);
+		BOOST_CHECK_EQUAL(t.num(),5);
+		BOOST_CHECK_EQUAL(t.den(),1);
+		BOOST_CHECK(t.num().get_symbol_set().size() == 0u);
+		BOOST_CHECK(t.den().get_symbol_set().size() == 0u);
+		BOOST_CHECK_EQUAL(s.num(),0);
+		BOOST_CHECK_EQUAL(s.den(),0);
+		BOOST_CHECK(s.num().get_symbol_set().size() == 0u);
+		BOOST_CHECK(s.den().get_symbol_set().size() == 0u);
+		// Revive.
+		s = t;
+		BOOST_CHECK_EQUAL(s.num(),5);
+		BOOST_CHECK_EQUAL(s.den(),1);
+		BOOST_CHECK(s.num().get_symbol_set().size() == 0u);
+		BOOST_CHECK(s.den().get_symbol_set().size() == 0u);
+		// Unary ctors.
 		BOOST_CHECK((std::is_constructible<r_type,p_type>::value));
 		BOOST_CHECK((std::is_constructible<r_type,p_type &>::value));
 		BOOST_CHECK((std::is_constructible<r_type,p_type &&>::value));
+		BOOST_CHECK((std::is_constructible<r_type,q_type>::value));
+		BOOST_CHECK((std::is_constructible<r_type,q_type &>::value));
+		BOOST_CHECK((std::is_constructible<r_type,q_type &&>::value));
 		BOOST_CHECK((std::is_constructible<r_type,int>::value));
 		BOOST_CHECK((std::is_constructible<r_type,char &>::value));
 		BOOST_CHECK((std::is_constructible<r_type,const integer &>::value));
@@ -69,10 +103,46 @@ struct constructor_tester
 		BOOST_CHECK((!std::is_constructible<r_type,double>::value));
 		BOOST_CHECK((!std::is_constructible<r_type,const float>::value));
 		BOOST_CHECK((!std::is_constructible<r_type,real &&>::value));
+		r = r_type{1};
+		BOOST_CHECK_EQUAL(r.num(),1);
+		BOOST_CHECK_EQUAL(r.den(),1);
+		BOOST_CHECK(r.num().get_symbol_set().size() == 0u);
+		BOOST_CHECK(r.den().get_symbol_set().size() == 0u);
+		r = -2;
+		BOOST_CHECK_EQUAL(r.num(),-2);
+		BOOST_CHECK_EQUAL(r.den(),1);
+		BOOST_CHECK(r.num().get_symbol_set().size() == 0u);
+		BOOST_CHECK(r.den().get_symbol_set().size() == 0u);
+
 		std::cout << r_type{q_type{"x"}+q_type{"x"}.pow(2)/2} << '\n';
 		std::cout << r_type{123_z} << '\n';
 		std::cout << r_type{122/244_q} << '\n';
 		std::cout << r_type{-122/244_q} << '\n';
+		// Binary ctors.
+		BOOST_CHECK((std::is_constructible<r_type,p_type,p_type>::value));
+		BOOST_CHECK((std::is_constructible<r_type,p_type &,p_type>::value));
+		BOOST_CHECK((std::is_constructible<r_type,p_type &&,const p_type &>::value));
+		BOOST_CHECK((std::is_constructible<r_type,q_type,q_type>::value));
+		BOOST_CHECK((std::is_constructible<r_type,q_type &,q_type>::value));
+		BOOST_CHECK((std::is_constructible<r_type,q_type &&,const q_type &>::value));
+		BOOST_CHECK((std::is_constructible<r_type,int,int>::value));
+		BOOST_CHECK((std::is_constructible<r_type,char &,char>::value));
+		BOOST_CHECK((std::is_constructible<r_type,const integer &,integer>::value));
+		BOOST_CHECK((std::is_constructible<r_type,rational, rational>::value));
+		BOOST_CHECK((std::is_constructible<r_type,const rational &, rational &&>::value));
+		BOOST_CHECK((std::is_constructible<r_type,std::string,std::string>::value));
+		BOOST_CHECK((std::is_constructible<r_type,std::string,std::string &>::value));
+		BOOST_CHECK((std::is_constructible<r_type,char *,char*>::value));
+		BOOST_CHECK((std::is_constructible<r_type,const char *,const char*>::value));
+		BOOST_CHECK((!std::is_constructible<r_type,double,double>::value));
+		BOOST_CHECK((!std::is_constructible<r_type,const float,float>::value));
+		BOOST_CHECK((!std::is_constructible<r_type,real &&,real>::value));
+		std::cout << r_type{1_z,-2_z} << '\n';
+		std::cout << r_type{(x+y)*(x-y),(2*x+2*y)*z} << '\n';
+		std::cout << r_type{(x+y)*(x-y),(2*x+2*y)*-z} << '\n';
+		std::cout << r_type{(xq+yq)*(xq-yq),(2*xq+2*yq)*-zq} << '\n';
+		std::cout << r_type{4*(xq+yq)*(xq-yq),2*(2*xq+2*yq)*-zq} << '\n';
+		// Assignments.
 	}
 };
 
