69ac3c5c9648b89ca1bb1a6942521816a2635f83
Francesco Biscani <bluescarni@gmail.com>
2016-10-06 00:13:19 +0200

    series: remove a soon-to-be-obsolete test that causes a failure in appveyor.

--- tests/series_03.cpp
+++ tests/series_03.cpp
@@ -31,7 +31,6 @@ see https://www.gnu.org/licenses/. */
 #define BOOST_TEST_MODULE series_03_test
 #include <boost/test/unit_test.hpp>
 
-#include <boost/filesystem.hpp>
 #include <iostream>
 #include <stdexcept>
 #include <string>
@@ -44,27 +43,6 @@ see https://www.gnu.org/licenses/. */
 #include "../src/serialization.hpp"
 #include "../src/symbol.hpp"
 #include "../src/symbol_set.hpp"
-
-namespace bfs = boost::filesystem;
-
-// Small raii class for creating a tmp file.
-struct tmp_file {
-    tmp_file()
-    {
-        m_path = bfs::temp_directory_path();
-        // Concatenate with a unique filename.
-        m_path /= bfs::unique_path();
-    }
-    ~tmp_file()
-    {
-        bfs::remove(m_path);
-    }
-    std::string name() const
-    {
-        return m_path.string();
-    }
-    bfs::path m_path;
-};
 
 using namespace piranha;
 
@@ -327,36 +305,4 @@ BOOST_AUTO_TEST_CASE(series_extend_symbol_set_test)
     foo = null.extend_symbol_set(symbol_set{symbol{"y"}, symbol{"x"}, symbol{"z"}});
     BOOST_CHECK(foo.size() == 0u);
     BOOST_CHECK((symbol_set{symbol{"y"}, symbol{"x"}, symbol{"z"}} == foo.get_symbol_set()));
-}
-
-template <typename T, typename... Args>
-inline void checker(const T &s, Args... args)
-{
-    tmp_file f;
-    T::save(s, f.name(), args...);
-    BOOST_CHECK_EQUAL(T::load(f.name(), args...), s);
-}
-
-BOOST_AUTO_TEST_CASE(series_save_load_test)
-{
-    using st0 = g_series_type<double, int>;
-    st0 x{"x"}, y{"y"};
-    checker(x);
-    checker(y);
-    checker(x, file_compression::bzip2);
-    checker(y, file_compression::bzip2);
-    checker(x, file_format::binary);
-    checker(y, file_format::binary);
-    checker(x, file_format::binary, file_compression::bzip2);
-    checker(y, file_format::binary, file_compression::bzip2);
-    // Try with non-existing file.
-    BOOST_CHECK_THROW(st0::load("123456.hhhh"), std::runtime_error);
-    // Try with a somewhat larger example.
-    {
-        using p_type = polynomial<rational, k_monomial>;
-        tmp_file f;
-        auto s = (p_type{"x"} + p_type{"y"}).pow(10);
-        p_type::save(s, f.name(), file_compression::bzip2);
-        BOOST_CHECK_EQUAL(p_type::load(f.name(), file_compression::bzip2), s);
-    }
 }
