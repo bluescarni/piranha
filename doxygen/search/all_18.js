bd4a1dfefbeae33e44ff236306124072317228da
Travis CI <bluescarni@gmail.com>
2016-10-24 20:34:07 +0000

    Update Doxygen documentation, commit 49505c6d78cbb7d975f240a1f6efd1949efe27ba [skip ci].

--- doxygen/search/all_18.js
+++ doxygen/search/all_18.js
@@ -2,5 +2,9 @@ var searchData=
 [
   ['zero_5fdivision_5ferror',['zero_division_error',['../structpiranha_1_1zero__division__error.html',1,'piranha']]],
   ['zero_5fdivision_5ferror',['zero_division_error',['../structpiranha_1_1zero__division__error.html#a38882aa0495b251357fc0abc80e1ea3b',1,'piranha::zero_division_error']]],
+  ['zero_5fis_5fabsorbing',['zero_is_absorbing',['../structpiranha_1_1zero__is__absorbing.html',1,'piranha']]],
+  ['zero_5fis_5fabsorbing_3c_20t_2c_20fp_5fzero_5fis_5fabsorbing_5fenabler_3c_20t_20_3e_20_3e',['zero_is_absorbing&lt; T, fp_zero_is_absorbing_enabler&lt; T &gt; &gt;',['../structpiranha_1_1zero__is__absorbing_3_01_t_00_01fp__zero__is__absorbing__enabler_3_01_t_01_4_01_4.html',1,'piranha']]],
+  ['zero_5fis_5fabsorbing_3c_20t_2c_20real_5fzero_5fis_5fabsorbing_5fenabler_3c_20t_20_3e_20_3e',['zero_is_absorbing&lt; T, real_zero_is_absorbing_enabler&lt; T &gt; &gt;',['../structpiranha_1_1zero__is__absorbing_3_01_t_00_01real__zero__is__absorbing__enabler_3_01_t_01_4_01_4.html',1,'piranha']]],
+  ['zero_5fis_5fabsorbing_3c_20t_2c_20series_5fzero_5fis_5fabsorbing_5fenabler_3c_20t_20_3e_20_3e',['zero_is_absorbing&lt; T, series_zero_is_absorbing_enabler&lt; T &gt; &gt;',['../structpiranha_1_1zero__is__absorbing_3_01_t_00_01series__zero__is__absorbing__enabler_3_01_t_01_4_01_4.html',1,'piranha']]],
   ['zlib',['zlib',['../namespacepiranha.html#a6acfbeb0c26b634e270f69f0ab13126ea7a990d405d2c6fb93aa8fbb0ec1a3b23',1,'piranha']]]
 ];
