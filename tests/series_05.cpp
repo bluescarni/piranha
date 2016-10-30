43b5dd2eaa1a2b4185da4c96a1022634b4be5552
Francesco Biscani <bluescarni@gmail.com>
2016-10-30 21:15:57 +0100

    Revert "Tentative fix for test warning spotted by appveyor."
    This reverts commit e215b314ff1c66fbaa668adb95c18105e2dfd487.

--- tests/series_05.cpp
+++ tests/series_05.cpp
@@ -37,7 +37,6 @@ see https://www.gnu.org/licenses/. */
 #include <type_traits>
 #include <unordered_map>
 
-#include "../src/config.hpp"
 #include "../src/exceptions.hpp"
 #include "../src/forwarding.hpp"
 #include "../src/init.hpp"
@@ -46,11 +45,6 @@ see https://www.gnu.org/licenses/. */
 #include "../src/mp_integer.hpp"
 #include "../src/polynomial.hpp"
 #include "../src/type_traits.hpp"
-
-#if defined(PIRANHA_COMPILER_IS_GCC)
-#pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wconversion"
-#endif
 
 using namespace piranha;
 
@@ -304,7 +298,3 @@ BOOST_AUTO_TEST_CASE(series_evaluation_test)
                     != std::string::npos);
     }
 }
-
-#if defined(PIRANHA_COMPILER_IS_GCC)
-#pragma GCC diagnostic pop
-#endif
