c07996c578b1543835acccb8732d5791f22f625c
Francesco Biscani <bluescarni@gmail.com>
2016-11-09 22:32:48 +0100

    Ditto.

--- tools/install_appveyor.py
+++ tools/install_appveyor.py
@@ -1,6 +1,9 @@
+import urllib.request
 import os
 
-def run_unbuffered_command(raw_command, directory):
+def run_unbuffered_command(raw_command, directory = None, verbose = True):
+    # Helper function to run a command and display optionally its output
+    # unbuffered.
     import shlex
     from subprocess import Popen, PIPE, STDOUT
     proc = Popen(shlex.split(raw_command), cwd=directory,
@@ -10,13 +13,16 @@ def run_unbuffered_command(raw_command, directory):
         line = proc.stdout.readline()
         if not line:
             break
-        print(str(line,'utf-8'))
+        if verbose:
+            print(str(line,'utf-8'))
         output.append(line)
     proc.communicate()
     if proc.returncode:
         raise RuntimeError(output)
     return output
 
-import urllib.request
-urllib.request.urlretrieve('https://github.com/bluescarni/binary_deps/raw/master/x86_64-6.2.0-release-posix-seh-rt_v5-rev1.7z', 'mw64.7z')
-run_unbuffered_command('7z x -oC:\ mw64.7z > NUL', os.getcwd())
+# Get mingw and set the path.
+urllib.request.urlretrieve(r'https://github.com/bluescarni/binary_deps/raw/master/x86_64-6.2.0-release-posix-seh-rt_v5-rev1.7z', 'mw64.7z')
+run_unbuffered_command(r'7z x -oC:\\ mw64.7z > NUL', verbose = False)
+os.environ['PATH'] = r'C:\\mingw64\\bin;%PATH%'
+print(os.environ['PATH'])
