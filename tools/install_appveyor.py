b0ce9e9a34294bb2ddb5810d27190f6873f779f0
Francesco Biscani <bluescarni@gmail.com>
2016-11-12 01:25:13 +0100

    Missing bit.

--- tools/install_appveyor.py
+++ tools/install_appveyor.py
@@ -69,6 +69,8 @@ is_python_build = 'Python' in BUILD_TYPE
 if is_python_build:
     if BUILD_TYPE == 'Python35':
         python_version = '35'
+    elif BUILD_TYPE == 'Python27':
+        python_version = '27'
     else:
         raise RuntimeError('Unsupported Python build: ' + BUILD_TYPE)
     python_package = r'python' + python_version + r'_mingw_64.7z'
