20b149439b3f09a74d6f7de171b4fc13a25ac03f
Francesco Biscani <bluescarni@gmail.com>
2016-11-13 12:29:48 +0100

    Wrap a test in a try/catch block in an attempt to debug a travis failure.

--- tools/install_appveyor.py
+++ tools/install_appveyor.py
@@ -65,7 +65,6 @@ os.environ['PATH'] = os.environ['PATH'] + r';c:\\local\\lib'
 # Build type setup.
 BUILD_TYPE = os.environ['BUILD_TYPE']
 is_master_build = (os.environ['APPVEYOR_REPO_BRANCH'] == 'master') and not os.environ['APPVEYOR_PULL_REQUEST_NUMBER']
-print(os.environ['APPVEYOR_PULL_REQUEST_NUMBER'])
 if is_master_build:
     print("Master build detected.")
 is_python_build = 'Python' in BUILD_TYPE
