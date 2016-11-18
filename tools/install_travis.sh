134cb875a0967cffa82c56c41516a32ce1b2cc33
Francesco Biscani <bluescarni@gmail.com>
2016-11-18 09:25:15 +0100

    travis fix.

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -44,7 +44,7 @@ fi
 
 if [[ "${BUILD_TYPE}" == "Python2" || "${BUILD_TYPE}" == "Python3" ]]; then
     cmake -DPIRANHA_WITH_MSGPACK=yes -DPIRANHA_WITH_BZIP2=yes -DPIRANHA_WITH_ZLIB=yes -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DBUILD_PYRANHA=yes -DCMAKE_CXX_FLAGS_DEBUG=-g0 -DCMAKE_CXX_FLAGS=-Os -DCMAKE_INSTALL_PREFIX=$deps_dir  ../;
-    make install VERBOSE = 1;
+    make install VERBOSE=1;
     python -c "import pyranha.test; pyranha.test.run_test_suite()";
 fi
 
