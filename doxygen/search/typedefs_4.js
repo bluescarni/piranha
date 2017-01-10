d0cee4cba5801bf56c5d41510c1ef1f9867c9b1f
Travis CI <bluescarni@gmail.com>
2017-01-10 16:56:25 +0000

    Update Doxygen documentation, commit 0ebc8702a36fcf14b3d83caa43c26a12f77ababf [skip ci].

--- doxygen/search/typedefs_4.js
+++ doxygen/search/typedefs_4.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['eval_5ftype',['eval_type',['../classpiranha_1_1math_1_1lambdified.html#a3e7d7ce74a488b58c055a8f819d07122',1,'piranha::math::lambdified']]],
-  ['extra_5fmap_5ftype',['extra_map_type',['../classpiranha_1_1math_1_1lambdified.html#ae09a4179fc240d1c83b2268e4c8bb08a',1,'piranha::math::lambdified']]]
+  ['extra_5fmap_5ftype',['extra_map_type',['../classpiranha_1_1math_1_1lambdified.html#a8a37396a62ec2f0f32995f991104ac94',1,'piranha::math::lambdified']]]
 ];
