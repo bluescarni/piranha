03b6215610a250cc1557a751b0799c6c5b8686e3
Francesco Biscani <bluescarni@gmail.com>
2017-01-23 01:16:03 +0100

    clang-format.

--- tools/sample_project/main.cpp
+++ tools/sample_project/main.cpp
@@ -4,7 +4,7 @@ using namespace piranha;
 
 int main()
 {
-	using p_type = polynomial<double,monomial<int>>;
-	p_type x{"x"};
-	save_file(x, "tmp.mpackp.bz2");
+    using p_type = polynomial<double, monomial<int>>;
+    p_type x{"x"};
+    save_file(x, "tmp.mpackp.bz2");
 }
