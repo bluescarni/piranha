a2ddeefe8949a1f36e0f051fccab74da520af2da
Francesco Biscani <bluescarni@gmail.com>
2018-02-08 09:28:28 +0100

    [skip appveyor] Don't run any thread test on travis.

--- tools/install_travis.sh
+++ tools/install_travis.sh
@@ -14,11 +14,11 @@ if [[ "${PIRANHA_BUILD}" == "DebugGCC48" ]]; then
 elif [[ "${PIRANHA_BUILD}" == "DebugGCC7" ]]; then
     CXX=g++-7 CC=gcc-7 cmake -DCMAKE_CXX_STANDARD=17 -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DPIRANHA_BUILD_TESTS=yes -DPIRANHA_WITH_BOOST_S11N=yes -DPIRANHA_WITH_BZIP2=yes -DPIRANHA_WITH_MSGPACK=yes -DPIRANHA_WITH_ZLIB=yes -DCMAKE_CXX_FLAGS_DEBUG="-g0 -Os -fconcepts" -DPIRANHA_TEST_NSPLIT=${TEST_NSPLIT} -DPIRANHA_TEST_SPLIT_NUM=${SPLIT_TEST_NUM} ../;
     make VERBOSE=1;
-    ctest -V;
+    ctest -E "thread" -V;
 elif [[ "${PIRANHA_BUILD}" == "DebugClang39" ]]; then
     CXX=clang++-3.9 CC=clang-3.9 cmake -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Debug -DPIRANHA_BUILD_TESTS=yes -DPIRANHA_WITH_BOOST_S11N=yes -DPIRANHA_WITH_BZIP2=yes -DPIRANHA_WITH_MSGPACK=yes -DPIRANHA_WITH_ZLIB=yes -DPIRANHA_TEST_NSPLIT=${TEST_NSPLIT} -DPIRANHA_TEST_SPLIT_NUM=${SPLIT_TEST_NUM} -DQuadmath_INCLUDE_DIR=/usr/lib/gcc/x86_64-linux-gnu/7/include -DQuadmath_LIBRARY=/usr/lib/gcc/x86_64-linux-gnu/7/libquadmath.so ../;
     make VERBOSE=1;
-    ctest -V;
+    ctest -E "thread" -V;
 fi
 
 set +e
