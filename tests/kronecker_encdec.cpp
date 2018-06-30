3ed04aed89f0149e082936a89d1fe206a1cc7790
Francesco Biscani <bluescarni@gmail.com>
2018-06-30 14:50:22 +0200

    More testing.

--- tests/kronecker_encdec.cpp
+++ tests/kronecker_encdec.cpp
@@ -31,8 +31,10 @@ see https://www.gnu.org/licenses/. */
 #define BOOST_TEST_MODULE kronecker_encdec_test
 #include <boost/test/included/unit_test.hpp>
 
+#include <algorithm>
 #include <cstddef>
 #include <cstdint>
+#include <iterator>
 #include <limits>
 #include <random>
 #include <stdexcept>
@@ -86,6 +88,7 @@ BOOST_AUTO_TEST_CASE(kronecker_limits_test)
     tuple_for_each(int_types{}, limits_tester{});
 }
 
+// Error handling in the streaming encoder/decoder.
 struct stream_errors_tester {
     template <typename T>
     void operator()(const T &) const
@@ -98,6 +101,11 @@ struct stream_errors_tester {
                                                  + std::to_string(l.size() - 1u));
         });
         k_encoder<T> k0{0};
+        BOOST_CHECK_EXCEPTION(k0 << T(1), std::out_of_range, [](const std::out_of_range &e) {
+            return boost::contains(
+                e.what(), "cannot push any more values to this Kronecker encoder: the number of "
+                          "values already pushed to the encoder is equal to the size used for construction (0)");
+        });
         BOOST_CHECK_EXCEPTION(k0 << T(1), std::out_of_range, [](const std::out_of_range &e) {
             return boost::contains(
                 e.what(), "cannot push any more values to this Kronecker encoder: the number of "
@@ -120,6 +128,15 @@ struct stream_errors_tester {
                                                + ", while the bounds are [" + std::to_string(-std::get<0>(l[2])[0])
                                                + ", " + std::to_string(std::get<0>(l[2])[0]) + "]");
                 });
+            BOOST_CHECK_EXCEPTION(
+                k2 << std::numeric_limits<T>::max(), std::overflow_error, [&l](const std::overflow_error &e) {
+                    return boost::contains(e.what(),
+                                           "one of the elements of a sequence to be Kronecker-encoded is out of "
+                                           "bounds: the value of the element is "
+                                               + std::to_string(std::numeric_limits<T>::max())
+                                               + ", while the bounds are [" + std::to_string(-std::get<0>(l[2])[0])
+                                               + ", " + std::to_string(std::get<0>(l[2])[0]) + "]");
+                });
         }
         k_encoder<T> k3{3};
         k3 << T(1);
@@ -128,12 +145,166 @@ struct stream_errors_tester {
                 e.what(), "cannot fetch the Kronecker-encoded value from this Kronecker encoder: the number of "
                           "values pushed to the encoder (1) is less than the size used for construction (3)");
         });
+        if (std::numeric_limits<T>::max() > std::get<1>(l[1])) {
+            BOOST_CHECK_EXCEPTION(
+                (k_decoder<T>{std::numeric_limits<T>::max(), 1}), std::overflow_error,
+                [&l](const std::overflow_error &e) {
+                    return boost::contains(
+                        e.what(),
+                        "cannot Kronecker-decode the signed integer " + std::to_string(std::numeric_limits<T>::max())
+                            + " of type '" + piranha::demangle<T>()
+                            + "' into a range of size 1: the value of the integer is outside the allowed bounds ["
+                            + std::to_string(std::get<1>(l[1])) + ", " + std::to_string(std::get<2>(l[1])) + "]");
+                });
+        }
+        k_decoder<T> d0{1, 1};
+        T out;
+        d0 >> out;
+        BOOST_CHECK_EXCEPTION(d0 >> out, std::out_of_range, [&l](const std::out_of_range &e) {
+            return boost::contains(e.what(), "cannot decode any more values from this Kronecker decoder: the number of "
+                                             "values already decoded is equal to the size used for construction (1)");
+        });
+        BOOST_CHECK_EXCEPTION(d0 >> out, std::out_of_range, [&l](const std::out_of_range &e) {
+            return boost::contains(e.what(), "cannot decode any more values from this Kronecker decoder: the number of "
+                                             "values already decoded is equal to the size used for construction (1)");
+        });
     }
 };
 
 BOOST_AUTO_TEST_CASE(kronecker_stream_errors)
 {
     tuple_for_each(int_types{}, stream_errors_tester{});
+}
+
+// Examples in the docs.
+BOOST_AUTO_TEST_CASE(kronecker_doctests)
+{
+    {
+        k_encoder<int> k(3);
+        k << 1 << 2 << 3;
+        auto code = k.get();
+        (void)code;
+    }
+    {
+        k_decoder<int> k(123, 3);
+        int a, b, c;
+        k >> a >> b >> c;
+    }
+    {
+        int v[] = {7, 8, 9};
+        auto code = k_encode<int>(v, 3);
+        (void)code;
+    }
+    {
+        int v[] = {7, 8, 9};
+        auto code = k_encode<int>(v, v + 3);
+        (void)code;
+    }
+    {
+        std::vector<long> v{1, 2, 3};
+        auto code = k_encode<long>(v);
+        (void)code;
+    }
+    {
+        int vec[3];
+        k_decode(42, vec, 3);
+    }
+    {
+        int vec[3];
+        k_decode(42, vec, vec + 3);
+    }
+    {
+        std::vector<int> vec(3);
+        k_decode(42, vec);
+        int arr[3];
+        k_decode(42, arr);
+        BOOST_CHECK(std::equal(arr, arr + 3, vec.begin()));
+    }
+}
+
+template <typename T, typename... Args>
+using k_encode_t = decltype(piranha::k_encode<T>(std::declval<Args>()...));
+
+template <typename... Args>
+using k_decode_t = decltype(piranha::k_decode(std::declval<Args>()...));
+
+// Concepts and type traits.
+BOOST_AUTO_TEST_CASE(kronecker_concepts_type_traits)
+{
+    // Uncv-ed C++ integral.
+    BOOST_CHECK((is_uncv_cpp_signed_integral<int>::value));
+    BOOST_CHECK((!is_uncv_cpp_signed_integral<int &>::value));
+    BOOST_CHECK((!is_uncv_cpp_signed_integral<const int>::value));
+    BOOST_CHECK((!is_uncv_cpp_signed_integral<volatile int>::value));
+    BOOST_CHECK((!is_uncv_cpp_signed_integral<unsigned>::value));
+    BOOST_CHECK((!is_uncv_cpp_signed_integral<void>::value));
+    BOOST_CHECK((!is_uncv_cpp_signed_integral<std::string>::value));
+    BOOST_CHECK((!is_uncv_cpp_signed_integral<double>::value));
+    // Encodable iterator.
+    BOOST_CHECK((is_k_encodable_iterator<std::vector<int>::const_iterator, long>::value));
+    BOOST_CHECK((is_k_encodable_iterator<std::vector<int>::iterator, long>::value));
+    BOOST_CHECK((is_k_encodable_iterator<int *, int>::value));
+    BOOST_CHECK((is_k_encodable_iterator<const double *, int>::value));
+    BOOST_CHECK((!is_k_encodable_iterator<std::vector<std::string>::const_iterator, long>::value));
+    BOOST_CHECK((!is_k_encodable_iterator<int *, double>::value));
+    BOOST_CHECK((!is_k_encodable_iterator<int *, void>::value));
+    BOOST_CHECK((is_k_encodable_iterator<std::istream_iterator<int>, int>::value));
+    BOOST_CHECK((!is_k_encodable_iterator<std::istream_iterator<std::string>, int>::value));
+    BOOST_CHECK((!is_k_encodable_iterator<std::istream_iterator<int>, void>::value));
+    // Encodable forward iterator.
+    BOOST_CHECK((is_k_encodable_forward_iterator<std::vector<int>::const_iterator, long>::value));
+    BOOST_CHECK((is_k_encodable_forward_iterator<std::vector<int>::iterator, long>::value));
+    BOOST_CHECK((is_k_encodable_forward_iterator<int *, int>::value));
+    BOOST_CHECK((is_k_encodable_forward_iterator<const double *, int>::value));
+    BOOST_CHECK((!is_k_encodable_forward_iterator<std::vector<std::string>::const_iterator, long>::value));
+    BOOST_CHECK((!is_k_encodable_forward_iterator<int *, double>::value));
+    BOOST_CHECK((!is_k_encodable_forward_iterator<int *, void>::value));
+    BOOST_CHECK((!is_k_encodable_forward_iterator<std::istream_iterator<int>, int>::value));
+    BOOST_CHECK((!is_k_encodable_forward_iterator<std::istream_iterator<std::string>, int>::value));
+    BOOST_CHECK((!is_k_encodable_forward_iterator<std::istream_iterator<int>, void>::value));
+    // Encodable forward range.
+    BOOST_CHECK((is_k_encodable_forward_range<std::vector<int>, long>::value));
+    BOOST_CHECK((!is_k_encodable_forward_range<void, long>::value));
+    BOOST_CHECK((!is_k_encodable_forward_range<std::vector<int>, void>::value));
+    BOOST_CHECK((is_k_encodable_forward_range<int(&)[3], long>::value));
+    BOOST_CHECK((!is_k_encodable_forward_range<std::vector<std::string>, long>::value));
+    // Decodable forward iterator.
+    BOOST_CHECK((is_k_decodable_forward_iterator<std::vector<int>::iterator, long>::value));
+    BOOST_CHECK((!is_k_decodable_forward_iterator<std::vector<int>::const_iterator, long>::value));
+    BOOST_CHECK((is_k_decodable_forward_iterator<int *, int>::value));
+    BOOST_CHECK((!is_k_decodable_forward_iterator<const int *, int>::value));
+    BOOST_CHECK((!is_k_decodable_forward_iterator<double *, int>::value));
+    BOOST_CHECK((!is_k_decodable_forward_iterator<std::vector<std::string>::iterator, long>::value));
+    BOOST_CHECK((!is_k_decodable_forward_iterator<int *, double>::value));
+    BOOST_CHECK((!is_k_decodable_forward_iterator<int *, void>::value));
+    BOOST_CHECK((!is_k_decodable_forward_iterator<std::ostream_iterator<int>, int>::value));
+    BOOST_CHECK((!is_k_decodable_forward_iterator<std::ostream_iterator<std::string>, int>::value));
+    BOOST_CHECK((!is_k_decodable_forward_iterator<std::istream_iterator<int>, void>::value));
+    // Decodable forward range.
+    BOOST_CHECK((is_k_decodable_forward_range<std::vector<int> &, long>::value));
+    BOOST_CHECK((!is_k_decodable_forward_range<const std::vector<int> &, long>::value));
+    BOOST_CHECK((!is_k_decodable_forward_range<void, long>::value));
+    BOOST_CHECK((!is_k_decodable_forward_range<std::vector<int> &, void>::value));
+    BOOST_CHECK((is_k_decodable_forward_range<int(&)[3], long>::value));
+    BOOST_CHECK((!is_k_decodable_forward_range<const int(&)[3], long>::value));
+    BOOST_CHECK((!is_k_decodable_forward_range<std::vector<std::string> &, long>::value));
+    // Misc tests.
+    BOOST_CHECK((is_detected<k_encode_t, int, int *, std::size_t>::value));
+    BOOST_CHECK((is_detected<k_encode_t, int, int *, int *>::value));
+    BOOST_CHECK((is_detected<k_encode_t, int, const std::vector<int> &>::value));
+    BOOST_CHECK((!is_detected<k_encode_t, double, int *, std::size_t>::value));
+    BOOST_CHECK((!is_detected<k_encode_t, int, std::string *, std::size_t>::value));
+    BOOST_CHECK((!is_detected<k_encode_t, int, std::string *, std::string *>::value));
+    BOOST_CHECK((!is_detected<k_encode_t, int, const std::vector<std::string> &>::value));
+    BOOST_CHECK((is_detected<k_decode_t, int, int *, std::size_t>::value));
+    BOOST_CHECK((is_detected<k_decode_t, int, int *, int *>::value));
+    BOOST_CHECK((is_detected<k_decode_t, int, std::vector<int> &>::value));
+    BOOST_CHECK((!is_detected<k_decode_t, int, const int *, std::size_t>::value));
+    BOOST_CHECK((!is_detected<k_decode_t, int, const int *, const int *>::value));
+    BOOST_CHECK((!is_detected<k_decode_t, int, const std::vector<int> &>::value));
+    BOOST_CHECK((!is_detected<k_decode_t, double, int *, std::size_t>::value));
+    BOOST_CHECK((!is_detected<k_decode_t, int, std::string *, std::string *>::value));
+    BOOST_CHECK((!is_detected<k_decode_t, int, std::vector<std::string> &>::value));
 }
 
 // Coding/decoding.
